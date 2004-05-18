90a66b715c376af68145162d326fd9409e881025
Marcin Konarski <marcin.konarski@codestation.org>
2004-05-18 23:39:28 +0000

    r413: New class `HComplex'.

---
+++ hcore/hcomplex.c
@@ -0,0 +1,198 @@
+/*
+---           `stdhapi' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+	hcomplex.c - this file is integral part of `stdhapi' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include "hexception.h"
+M_CVSID ( "$CVSHeader$" );
+#include "hcomplex.h"
+
+HComplex::HComplex ( void )
+	{
+	M_PROLOG
+	f_dReal = 0;
+	f_dImaginary = 0;
+	return;
+	M_EPILOG
+	}
+
+HComplex::HComplex ( double a_dReal,  double a_dImaginary )
+	{
+	M_PROLOG
+	f_dReal = a_dReal;
+	f_dImaginary = a_dImaginary;
+	return;
+	M_EPILOG
+	}
+
+HComplex::HComplex ( const HComplex & a_roComplex, int )
+	{
+	M_PROLOG
+	( * this ) = a_roComplex;
+	M_EPILOG
+	}
+
+HComplex::~HComplex ( void )
+	{
+	M_PROLOG
+	return;
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator = ( const HComplex & a_roComplex )
+	{
+	f_dReal = a_roComplex.f_dReal;
+	f_dImaginary = a_roComplex.f_dImaginary;
+	return ( * this );
+	}
+
+bool HComplex::operator == ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	return ( ( f_dReal == a_roComplex.f_dReal )
+			&& ( f_dImaginary == a_roComplex.f_dImaginary ) );
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator = ( const double a_dReal )
+	{
+	M_PROLOG
+	f_dReal = a_dReal;
+	f_dImaginary = 0;
+	return ( * this );
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator += ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	( * this ) = ( * this ) + a_roComplex;
+	return ( * this );
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator -= ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	( * this ) = ( * this ) - a_roComplex;
+	return ( * this );
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator *= ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	( * this ) = ( * this ) * a_roComplex;
+	return ( * this );
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator *= ( const double a_dValue )
+	{
+	M_PROLOG
+	( * this ) = ( * this ) * a_dValue;
+	return ( * this );
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator /= ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	( * this ) = ( * this ) / a_roComplex;
+	return ( * this );
+	M_EPILOG
+	}
+
+HComplex & HComplex::operator /= ( const double a_dValue )
+	{
+	M_PROLOG
+	( * this ) = ( * this ) / a_dValue;
+	return ( * this );
+	M_EPILOG
+	}
+
+HComplex HComplex::operator + ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	HComplex l_oComplex ( f_dReal, f_dImaginary );
+	l_oComplex.f_dReal += a_roComplex.f_dReal;
+	l_oComplex.f_dImaginary += a_roComplex.f_dImaginary;
+	return ( l_oComplex );
+	M_EPILOG
+	}
+
+HComplex HComplex::operator - ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	HComplex l_oComplex ( f_dReal, f_dImaginary );
+	l_oComplex.f_dReal -= a_roComplex.f_dReal;
+	l_oComplex.f_dImaginary -= a_roComplex.f_dImaginary;
+	return ( l_oComplex );
+	M_EPILOG
+	}
+
+HComplex HComplex::operator * ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	HComplex l_oComplex ( f_dReal, f_dImaginary );
+	l_oComplex.f_dReal *= a_roComplex.f_dReal - f_dImaginary * a_roComplex.f_dImaginary;
+	l_oComplex.f_dImaginary *= a_roComplex.f_dReal + f_dReal * a_roComplex.f_dImaginary;
+	return ( l_oComplex );
+	M_EPILOG
+	}
+
+HComplex HComplex::operator * ( const double a_dValue )
+	{
+	M_PROLOG
+	HComplex l_oComplex ( f_dReal, f_dImaginary );
+	l_oComplex.f_dReal *= a_dValue;
+	l_oComplex.f_dImaginary *= a_dValue;
+	return ( l_oComplex );
+	M_EPILOG
+	}
+
+HComplex HComplex::operator / ( const HComplex & a_roComplex )
+	{
+	M_PROLOG
+	HComplex l_oComplex ( f_dReal, f_dImaginary );
+	double l_dDenominator = a_roComplex.f_dReal * a_roComplex.f_dReal
+													+ a_roComplex.f_dImaginary * a_roComplex.f_dImaginary;
+	l_oComplex.f_dReal = ( f_dReal * a_roComplex.f_dReal
+			+ f_dImaginary * a_roComplex.f_dImaginary ) / l_dDenominator;
+	l_oComplex.f_dImaginary = ( a_roComplex.f_dReal * f_dImaginary
+			- f_dReal * a_roComplex.f_dImaginary ) / l_dDenominator;
+	return ( l_oComplex );
+	M_EPILOG
+	}
+
+HComplex HComplex::operator / ( const double a_dValue )
+	{
+	M_PROLOG
+	HComplex l_oComplex ( f_dReal, f_dImaginary );
+	l_oComplex.f_dReal /= a_dValue;
+	l_oComplex.f_dImaginary /= a_dValue;
+	return ( * this );
+	M_EPILOG
+	}
+
