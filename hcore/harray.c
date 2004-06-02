6420bbc037bf10e57fb7fd7c333a25579e35f8ec
Marcin Konarski <marcin.konarski@codestation.org>
2004-06-02 16:05:02 +0000

    r455: String for template compiled once.

---
+++ hcore/harray.c
@@ -0,0 +1,38 @@
+/*
+---           `stdhapi' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+	harray.c - this file is integral part of `stdhapi' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include "hexception.h"
+M_CVSID ( "$CVSHeader$" );
+#include "harray.h"
+M_CVSTID ( D_CVSID_HARRAY_H );
+
+const char * g_ppcErrMsgHArray [ 24 ] =
+	{
+/* E_BADSIZE */		_ ( "bad size" ),
+/* E_NOMEM */			_ ( "can not alloc memory with new" ),
+/* E_BADINDEX */	_ ( "index excides array size" )
+	};
+
