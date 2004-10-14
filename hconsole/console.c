0d2be2faf072862d6172b59dc516ca6d8ed68fe6
Marcin Konarski <marcin.konarski@codestation.org>
2004-10-14 12:51:28 +0000

    r553: Mouse events works under X also.

--- hconsole/console.c
+++ hconsole/console.c
@@ -364,8 +364,12 @@ int wait_for_user_input ( int & a_iKey, mouse::OMouse & a_sMouse,
 	if ( l_iError > 0 )
 		{
 		if ( FD_ISSET ( STDIN_FILENO, & l_xFdSet ) )
-			a_iKey = get_key ( ), l_iEventType |= D_EVENT_KEYBOARD;
-		if ( n_iMouseDes && FD_ISSET ( n_iMouseDes, & l_xFdSet ) )
+			{
+			a_iKey = get_key ( ), l_iEventType = D_EVENT_KEYBOARD;
+			if ( a_iKey == KEY_MOUSE )l_iEventType = 0;
+			}
+		if ( ( a_iKey == KEY_MOUSE )
+				|| ( n_iMouseDes && FD_ISSET ( n_iMouseDes, & l_xFdSet ) ) )
 			mouse::mouse_get ( a_sMouse ), l_iEventType |= D_EVENT_MOUSE;
 		}
 	return ( l_iEventType );
