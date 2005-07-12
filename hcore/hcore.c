e6f97effc1384d9ac49e5b011674eaf9e5ad01c5
Marcin Konarski <marcin.konarski@codestation.org>
2005-07-12 12:07:40 +0000

    r944: Copyright notice added to library version string.

--- hcore/hcore.c
+++ hcore/hcore.c
@@ -1,28 +1,29 @@
-/*
----           `stdhapi' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
-
-	hcore.c - this file is integral part of `stdhapi' project.
-
-	i.  You may not make any changes in Copyright information.
-	ii. You must attach Copyright information to any part of every copy
-	    of this software.
-
-Copyright:
-
- You are free to use this program as is, you can redistribute binary
- package freely but:
-  1. You can not use any part of sources of this software.
-  2. You can not redistribute any part of sources of this software.
-  3. No reverse engineering is allowed.
-  4. If you want redistribute binary package you can not demand any fees
-     for this software.
-     You can not even demand cost of the carrier (CD for example).
-  5. You can not include it to any commercial enterprise (for example 
-     as a free add-on to payed software or payed newspaper).
- This program is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
-*/
+char const COPYRIGHT [ ] =
+"/*\n"
+"---           `stdhapi' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---\n"
+"\n"
+/* hcore.c - this file is integral part of `stdhapi' project.
+ */
+"	i.  You may not make any changes in Copyright information.\n"
+"	ii. You must attach Copyright information to any part of every copy\n"
+"	    of this software.\n"
+"\n"
+"Copyright:\n"
+"\n"
+" You are free to use this program as is, you can redistribute binary\n"
+" package freely but:\n"
+"  1. You can not use any part of sources of this software.\n"
+"  2. You can not redistribute any part of sources of this software.\n"
+"  3. No reverse engineering is allowed.\n"
+"  4. If you want redistribute binary package you can not demand any fees\n"
+"     for this software.\n"
+"     You can not even demand cost of the carrier (CD for example).\n"
+"  5. You can not include it to any commercial enterprise (for example\n"
+"     as a free add-on to payed software or payed newspaper).\n"
+" This program is distributed in the hope that it will be useful, but WITHOUT\n"
+" ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or\n"
+" FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.\n"
+"*/\n";
 
 #include <stdlib.h>
 #include <string.h>
@@ -138,9 +139,10 @@ void hcore_init ( void )
 
 void stdhapi_hcore_banner ( void )
 	{
-	fprintf ( stderr, "stdhapi version 0.0.0\n"
+	fprintf ( stderr,
+			"%s\n"
 			"Available libraries:\n"
-			"\thcore\n"	);
+			"\thcore\n", COPYRIGHT );
 	return;
 	}
 
