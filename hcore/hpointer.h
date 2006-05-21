67c93f733729bc857b8ff0bdd6b7b80a7beb7d0c
Marcin Konarski <marcin.konarski@codestation.org>
2006-05-21 20:18:59 +0000

    r1374: Behaviour of HPointer is now steered by policies.

--- hcore/hpointer.h
+++ hcore/hpointer.h
@@ -42,14 +42,38 @@ namespace hcore
 /* Due to extream vitality of this class,
  * none of the methods are guarded. */
 
-template < typename tType, bool array = false >
+template < typename tType >
+struct HPointerScalar
+	{
+	static void delete_pointee ( tType * );
+	};
+
+template < typename tType >
+struct HPointerArray
+	{
+	static void delete_pointee ( tType * );
+	static tType & object_at ( tType *, int );
+	};
+
+template < typename tType >
+struct HPointerStrict
+	{
+	};
+
+template < typename tType >
+struct HPointerRelaxed
+	{
+	static tType * raw ( tType * );
+	};
+
+template < typename tType, template < typename > class pointer_type_t = HPointerScalar,
+				 template < typename > class access_type_t = HPointerStrict >
 class HPointer
 	{
 	class HShared
 		{
 		tType * f_ptPointer;
 		int f_iReferenceCounter;
-		bool f_bArray;
 		explicit HShared ( tType * const );
 		virtual ~HShared ( void ) throw ( );
 		bool release ( void ) throw ( );
@@ -68,67 +92,98 @@ public:
 	HPointer ( HPointer const & );
 	HPointer & operator = ( HPointer const & );
 	tType & operator * ( void ) const;
-	tType * operator-> ( void ) const;
 	tType & operator [ ] ( int ) const;
+	bool operator == ( HPointer const & ) const;
+	bool operator != ( HPointer const & ) const;
+	tType * operator-> ( void ) const;
+	tType * raw ( void ) const;
 	/*}*/
 	};
 
-template < typename tType, bool array >
-HPointer < tType, array >::HShared::HShared ( tType * const a_ptPointer )
-	: f_ptPointer ( a_ptPointer ), f_iReferenceCounter ( 1 ), f_bArray ( array )
+template < typename tType >
+void HPointerScalar < tType >::delete_pointee ( tType * a_ptPointer )
+	{
+	delete a_ptPointer;
+	}
+
+template < typename tType >
+void HPointerArray < tType >::delete_pointee ( tType * a_ptPointer )
+	{
+	delete [ ] a_ptPointer;
+	}
+
+template < typename tType >
+tType & HPointerArray < tType >::object_at ( tType * a_ptPointer, int a_iIndex )
+	{
+	return ( a_ptPointer [ a_iIndex ] );
+	}
+
+template < typename tType >
+tType * HPointerRelaxed < tType >::raw ( tType * a_ptPointer )
+	{
+	return ( a_ptPointer );
+	}
+
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+HPointer < tType, pointer_type_t, access_type_t >::HShared::HShared ( tType * const a_ptPointer )
+	: f_ptPointer ( a_ptPointer ), f_iReferenceCounter ( 1 )
 	{
 	M_ASSERT ( f_ptPointer );
 	return;
 	}
 
-template < typename tType, bool array >
-HPointer < tType, array >::HShared::~HShared ( void ) throw ( )
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class  access_type_t >
+HPointer < tType, pointer_type_t, access_type_t >::HShared::~HShared ( void ) throw ( )
 	{
 	M_ASSERT ( f_iReferenceCounter == 0 );
 	return;
 	}
 
-template < typename tType, bool array >
-bool HPointer < tType, array >::HShared::release ( void ) throw ( )
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+bool HPointer < tType, pointer_type_t, access_type_t >::HShared::release ( void ) throw ( )
 	{
 	M_ASSERT ( f_iReferenceCounter > 0 );
 	f_iReferenceCounter --;
 	if ( ! f_iReferenceCounter )
 		{
-		if ( array )
-			delete [ ] f_ptPointer;
-		else
-			delete f_ptPointer;
+		pointer_type_t<tType>::delete_pointee ( f_ptPointer );
 		f_ptPointer = NULL;
 		}
 	return ( ! f_iReferenceCounter );
 	}
 
-template < typename tType, bool array >
-HPointer < tType, array >::HPointer ( tType * const a_ptPointer )
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+HPointer < tType, pointer_type_t, access_type_t >::HPointer ( tType * const a_ptPointer )
 	: f_poShared ( new HShared ( a_ptPointer ) )
 	{
 	return;
 	}
 
-template < typename tType, bool array >
-HPointer < tType, array >::~HPointer ( void )
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+HPointer < tType, pointer_type_t, access_type_t >::~HPointer ( void )
 	{
 	if ( f_poShared->release ( ) )
 		delete f_poShared;
 	return;
 	}
 
-template < typename tType, bool array >
-HPointer < tType, array >::HPointer ( HPointer const & a_roPointer )
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+HPointer < tType, pointer_type_t, access_type_t >::HPointer ( HPointer const & a_roPointer )
 	: f_poShared ( NULL )
 	{
 	operator = ( a_roPointer );
 	return;
 	}
 
-template < typename tType, bool array >
-HPointer < tType, array > & HPointer < tType, array >::operator = ( HPointer const & a_roPointer )
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+HPointer < tType, pointer_type_t, access_type_t > & HPointer < tType, pointer_type_t, access_type_t >::operator = ( HPointer const & a_roPointer )
 	{
 	if ( ( & a_roPointer != this ) && ( a_roPointer.f_poShared != f_poShared ) )
 		{
@@ -144,24 +199,47 @@ HPointer < tType, array > & HPointer < tType, array >::operator = ( HPointer con
 	return ( * this );
 	}
 
-template < typename tType, bool array >
-tType & HPointer < tType, array >::operator * ( void ) const
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+tType & HPointer < tType, pointer_type_t, access_type_t >::operator * ( void ) const
 	{
 	return ( * f_poShared->f_ptPointer );
 	}
 
-template < typename tType, bool array >
-tType * HPointer < tType, array >::operator-> ( void ) const
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+tType & HPointer < tType, pointer_type_t, access_type_t >::operator [ ] ( int a_iIndex ) const
 	{
-	return ( f_poShared->f_ptPointer );
+	M_ASSERT ( a_iIndex >= 0 );
+	return ( pointer_type_t<tType>::object_at ( f_poShared->f_ptPointer, a_iIndex ) );
 	}
 
-template < typename tType, bool array >
-tType & HPointer < tType, array >::operator [ ] ( int a_iIndex ) const
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+bool HPointer < tType, pointer_type_t, access_type_t >::operator == ( HPointer < tType, pointer_type_t, access_type_t > const & a_tPointer ) const
 	{
-	M_ASSERT ( f_poShared->f_bArray );
-	M_ASSERT ( a_iIndex >= 0 );
-	return ( f_poShared->f_ptPointer [ a_iIndex ] );
+	return ( f_poShared->f_ptPointer == a_tPointer.f_poShared->f_ptPointer );
+	}
+
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+bool HPointer < tType, pointer_type_t, access_type_t >::operator != ( HPointer < tType, pointer_type_t, access_type_t > const & a_tPointer ) const
+	{
+	return ( f_poShared->f_ptPointer != a_tPointer.f_poShared->f_ptPointer );
+	}
+
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+tType * HPointer < tType, pointer_type_t, access_type_t >::operator-> ( void ) const
+	{
+	return ( access_type_t<tType>::raw ( f_poShared->f_ptPointer ) );
+	}
+
+template < typename tType, template < typename > class pointer_type_t,
+				 template < typename > class access_type_t >
+tType * HPointer < tType, pointer_type_t, access_type_t >::raw ( void ) const
+	{
+	return ( access_type_t<tType>::raw ( f_poShared->f_ptPointer ) );
 	}
 
 }
