d5e719fa8312574cfe9670fe3a731e69d3235bda
Marcin Konarski <marcin.konarski@codestation.org>
2006-07-11 21:48:56 +0000

    r1407: Strating of the implementation of the binary search tree (BST).

---
+++ hcore/hbtree.c
@@ -0,0 +1,52 @@
+/*
+---           `stdhapi' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+	hbtree.c - this file is integral part of `stdhapi' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <libintl.h>
+
+#include "hexception.h"
+M_CVSID ( "$CVSHeader$" )
+#include "hbtree.h"
+M_CVSTID ( D_CVSID_HBTREE_H )
+
+namespace stdhapi
+{
+
+namespace hcore
+{
+
+char const * const g_ppcErrMsgHBTree [ 4 ] =
+	{
+	_ ( "ok" ),
+/* HBTree::ERROR::E_BADSIZE */		_ ( "bad size" ),
+/* HBTree::ERROR::E_NOMEM */			_ ( "can not alloc memory with new" ),
+/* HBTree::ERROR::E_BADINDEX */		_ ( "index does not fit into array" )
+	};
+
+}
+
+}
+
+
---
+++ hcore/hbtree.h
@@ -0,0 +1,95 @@
+/*
+---           `stdhapi' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+	hbtree.h - this file is integral part of `stdhapi' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#ifndef __STDHAPI_HCORE_HBTREE_H
+#define __STDHAPI_HCORE_HBTREE_H
+
+#line 31
+
+#include "hexception.h"
+
+namespace stdhapi
+{
+
+namespace hcore
+{
+
+#define D_CVSID_HBTREE_H "$CVSHeader$"
+
+template < typename tType >
+class HBTree
+	{
+protected:
+	/*{*/
+	class HNode
+		{
+	protected:
+		/*{*/
+		HNode * f_poParent;
+		HNode * f_poLeft;
+		HNode * f_poRight;
+		tType f_tKey;
+		/*}*/
+	public:
+		/*{*/
+		HNode ( void );
+		virtual ~HNode ( void );
+		/*}*/
+	protected:
+		/*{*/
+		/*}*/
+	private:
+		/*{*/
+		HNode ( HNode const & );
+		HNode & operator = ( HNode const & );
+		/*}*/
+		};
+	/*}*/
+public:
+	/*{*/
+	virtual ~HBTree ( void );
+	void insert ( tType const & );
+	void remove ( tType const & );
+	void find ( tType const & );
+	/*}*/
+protected:
+	/*{*/
+	/*}*/
+	};
+
+template < typename tType >
+HBTree < tType >::HNode::HNode ( void )
+	: f_poParent ( NULL ), f_poLeft ( NULL ), f_poRight ( NULL ), f_tKey ( )
+	{
+	return;
+	}
+
+}
+
+}
+
+#endif /* not __STDHAPI_HCORE_HBTREE_H */
+
