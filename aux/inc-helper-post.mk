0c7dcae034f6a79ca720ec8b5624d07bbfe6a5fc
Marcin Konarski <marcin.konarski@codestation.org>
2008-03-29 06:19:55 +0000

    r2751: aux is restricted under windows

--- aux/inc-helper-post.mk
+++ aux/inc-helper-post.mk
@@ -1,9 +1,9 @@
-include $(DIR_ROOT)/aux/common-flags-post.mk
-include $(DIR_ROOT)/aux/common-int-libs.mk
-include $(DIR_ROOT)/aux/common-func.mk
-include $(DIR_ROOT)/aux/common-prepare-target.mk
-include $(DIR_ROOT)/aux/common-common-targets.mk
-include $(DIR_ROOT)/aux/common-filenames.mk
-include $(DIR_ROOT)/aux/common-target-default.mk
-include $(DIR_ROOT)/aux/common-target-tags.mk
+include $(DIR_ROOT)/_aux/common-flags-post.mk
+include $(DIR_ROOT)/_aux/common-int-libs.mk
+include $(DIR_ROOT)/_aux/common-func.mk
+include $(DIR_ROOT)/_aux/common-prepare-target.mk
+include $(DIR_ROOT)/_aux/common-common-targets.mk
+include $(DIR_ROOT)/_aux/common-filenames.mk
+include $(DIR_ROOT)/_aux/common-target-default.mk
+include $(DIR_ROOT)/_aux/common-target-tags.mk
 
--- aux/inc-helper-pre.mk
+++ aux/inc-helper-pre.mk
@@ -1,7 +1,7 @@
-include $(DIR_ROOT)/aux/common-basic.mk
-include $(DIR_ROOT)/aux/common-term.mk
-include $(DIR_ROOT)/aux/common-suffix.mk
-include $(DIR_ROOT)/aux/common-wrapper.mk
-include $(DIR_ROOT)/aux/common-flags.mk
-include $(DIR_ROOT)/aux/common-item-paths.mk
+include $(DIR_ROOT)/_aux/common-basic.mk
+include $(DIR_ROOT)/_aux/common-term.mk
+include $(DIR_ROOT)/_aux/common-suffix.mk
+include $(DIR_ROOT)/_aux/common-wrapper.mk
+include $(DIR_ROOT)/_aux/common-flags.mk
+include $(DIR_ROOT)/_aux/common-item-paths.mk
 
