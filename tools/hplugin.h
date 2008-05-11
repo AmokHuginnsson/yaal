15b5ba570e3061e9c5ce5810c374cb219eb42d05
Marcin Konarski <marcin.konarski@codestation.org>
2008-05-11 19:07:50 +0000

    r2974: skeleton for plugin implementation.

---
+++ tools/hplugin.h
@@ -0,0 +1,52 @@
+/*
+---           `yaal' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+	hplugin.h - this file is integral part of `yaal' project.
+
+	i.  You may not make any changes in Copyright information.
+	ii. You must attach Copyright information to any part of every copy
+	    of this software.
+
+Copyright:
+
+ You are free to use this program as is, you can redistribute binary
+ package freely but:
+  1. You can not use any part of sources of this software.
+  2. You can not redistribute any part of sources of this software.
+  3. No reverse engineering is allowed.
+  4. If you want redistribute binary package you can not demand any fees
+     for this software.
+     You can not even demand cost of the carrier (CD for example).
+  5. You can not include it to any commercial enterprise (for example 
+     as a free add-on to payed software or payed newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#ifndef __YAAL_TOOLS_HPLUGIN_H
+#define __YAAL_TOOLS_HPLUGIN_H
+
+namespace yaal
+{
+
+namespace tools
+{
+
+class HPlugin
+	{
+	void* f_pvHandle;
+public:
+	HPlugin( void );
+	~HPlugin( void );
+private:
+	HPlugin( HPlugin const& );
+	HPlugin const& operator = ( HPlugin const& );
+	};
+
+}
+
+}
+
+#endif /* not __YAAL_TOOLS_HPLUGIN_H */
+
