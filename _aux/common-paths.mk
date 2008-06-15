29d68de9454e52597b44d5c9eeccb69f8f32f77b
Marcin Konarski <marcin.konarski@codestation.org>
2008-06-15 21:18:47 +0200

    nicer way to specify multiple targets for client programmes.

--- _aux/common-paths.mk
+++ _aux/common-paths.mk
@@ -2,3 +2,17 @@ VPATH					= $(DIR_ROOT)/src
 DIR_DEST 			=
 prefix				= $(DIR_DEST)/usr/bin
 doc						= $(DIR_DEST)/usr/share/doc
+
+# this neat trick does not really blong here
+# but we want client makefiles look really simple
+
+TARGET_NO = 0
+define NEW_TARGET
+	TARGET_$$(TARGET_NO) := $(1)
+	SRC_TARGET_$$(TARGET_NO) := $(2)
+	COMPILER_FLAGS_TARGET_$$(TARGET_NO) := $(3)
+	LINKER_FLAGS_TARGET_$$(TARGET_NO) := $(4)
+	TARGETS := $$(TARGETS) TARGET_$$(TARGET_NO)
+	TARGET_NO := $$(TARGET_NO)0
+endef
+
