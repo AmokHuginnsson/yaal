d6c9e2b78a4e5b5e6a2e79be7b24b3f92ebe5d34
Marcin Konarski <marcin.konarski@codestation.org>
2008-11-15 14:50:24 +0100

    fix for GNU gethostby*_r interface simulation.

--- hcore/compat.cxx
+++ hcore/compat.cxx
@@ -59,9 +59,11 @@ char* strcasestr( char const* haystack, char const* needle )
 
 #if defined( HAVE_GETHOSTBYNAME_R )
 #if ! defined( HAVE_GNU_GETHOSTBYNAME_R )
-int gethostbyname_r( char const* a0, struct hostent* a1, char* a2, size_t a3, struct hostent**, int* a5 )
+int gethostbyname_r( char const* a0, struct hostent* a1, char* a2, size_t a3, struct hostent** a4, int* a5 )
 	{
 	hostent* h = ::gethostbyname_r( a0, a1, a2, a3, a5 );
+	if ( a4 )
+		*a4 = h;
 	return ( h ? 0 : errno );
 	}
 #endif /* not HAVE_GNU_GETHOSTBYNAME_R */
@@ -69,9 +71,11 @@ int gethostbyname_r( char const* a0, struct hostent* a1, char* a2, size_t a3, st
 
 #if defined( HAVE_GETHOSTBYADDR_R )
 #if ! defined( HAVE_GNU_GETHOSTBYADDR_R )
-int gethostbyaddr_r( void const* a0, int a1, int a2, struct hostent* a3, char* a4, size_t a5, struct hostent**, int* a7 )
+int gethostbyaddr_r( void const* a0, int a1, int a2, struct hostent* a3, char* a4, size_t a5, struct hostent** a6, int* a7 )
 	{
 	hostent* h = ::gethostbyaddr_r( static_cast<char const*>( a0 ), a1, a2, a3, a4, a5, a7 );
+	if ( a6 )
+		*a6 = h;
 	return ( h ? 0 : errno );
 	}
 #endif /* not HAVE_GNU_GETHOSTBYADDR_R */
