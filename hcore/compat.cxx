57ada7e7a4cbbb50ef7853d91bd163af6a65e813
Marcin Konarski <marcin.konarski@codestation.org>
2008-11-30 01:24:04 +0100

    Solaris compat.

--- hcore/compat.cxx
+++ hcore/compat.cxx
@@ -24,6 +24,10 @@ Copyright:
  FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
 */
 
+#include <cstdlib>
+
+#include "compat.h"
+
 #if ! defined( HAVE_POWL ) || ( HAVE_POWL == 0 ) || ! defined( HAVE_DECL_FLOORL ) || ( HAVE_DECL_FLOORL == 0 )
 #include <cmath>
 #endif /* not HAVE_POWL *//* not HAVE_DECL_FLOORL */
@@ -37,16 +41,10 @@ Copyright:
 #include <cstdarg>
 #endif /* not HAVE_ASPRINTF */
 
-#if ! defined( HAVE_STRTOLD ) || ( HAVE_STRTOLD == 0 )
-#include <cstdlib>
-#endif /* not HAVE_STRTOLD */
-
 #if ( defined( HAVE_GETHOSTBYNAME_R ) && ( ! defined( HAVE_GNU_GETHOSTBYNAME_R ) ) ) || ( defined( HAVE_GETHOSTBYADDR_R ) && ( ! defined( HAVE_GNU_GETHOSTBYADDR_R ) ) )
 #include <cerrno>
 #include <netdb.h>
 #endif /* ( HAVE_GETHOSTBYNAME_R && not HAVE_GNU_GETHOSTBYNAME_R ) || ( HAVE_GETHOSTBYADDR_R && not HAVE_GNU_GETHOSTBYADDR_R ) */
-
-#include "compat.h"
 
 #if ! defined( HAVE_STRCASESTR ) || ( HAVE_STRCASESTR == 0 )
 #include "hstring.h"
