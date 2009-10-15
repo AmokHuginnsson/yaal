c3aa1f1300b01acfe5d580b2e821b3ee2494d582
Marcin Konarski <marcin.konarski@codestation.org>
2009-10-15 16:16:46 +0200

    tags generation fixed.

--- _aux/mk/f_target-tags.mk
+++ _aux/mk/f_target-tags.mk
@@ -1,7 +1,5 @@
 tags: $(SRCS) $(HDRS)
 	@echo -n "Rehashing tags ... "; \
-	$(CTAGS)	-R --langmap="c++:.$(HS).$(SS)" -h ".$(HS)" --languages="c++" \
-						--c++-kinds="+p+x" --format=2 --fields="afikKlmnsSz" \
-						--extra="+f+q"; \
+	cd $(DIR_ROOT) && $(CTAGS) && \
 	echo "done."
 
