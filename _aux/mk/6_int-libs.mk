8f1bfe887397a63106cbeb48bec066162b11aa01
Marcin Konarski <marcin.konarski@codestation.org>
2009-12-27 06:49:29 +0100

    fix -l-l problem.

--- _aux/mk/6_int-libs.mk
+++ _aux/mk/6_int-libs.mk
@@ -1,6 +1,6 @@
 STATICLIBS := $(foreach SL,$(patsubst %,\%%.a,$(LIBS)),$(filter $(SL),$(wildcard ~/usr/lib/lib*.a)))
 SHAREDLIBS := $(patsubst %, -l%,$(filter-out $(patsubst ${HOME}/usr/lib/lib%.a,%,$(STATICLIBS)),$(LIBS)))
-LIBS := $(patsubst %, -l%, $(LIBS))
+LIBS := $(subst -l-l,-l,$(patsubst %, -l%, $(LIBS)))
 
 STATICLIBS += -rdynamic -ldl -lssl -lxslt -lxml2 -lpthread -lrt
 STATICLIBS += -lncurses -lgpm $(SHAREDLIBS)
