20f28254c7f150752e67a7080e6c9422d655cb7b
Marcin Konarski <marcin.konarski@codestation.org>
2010-05-25 12:43:38 +0200

    Pretty printer for HHashSet and HSet ready.

--- _aux/gdb-pretty-printer.py
+++ _aux/gdb-pretty-printer.py
@@ -30,9 +30,15 @@ def yaal_lookup_function( val ):
 	regex = re.compile( "^yaal::hcore::HMap<.*>$" )
 	if regex.match( lookup_tag ):
 		return YaalHCoreHMapPrinter( val )
+	regex = re.compile( "^yaal::hcore::HSet<.*>$" )
+	if regex.match( lookup_tag ):
+		return YaalHCoreHSetPrinter( val )
 	regex = re.compile( "^yaal::hcore::HHashMap<.*>$" )
 	if regex.match( lookup_tag ):
 		return YaalHCoreHHashMapPrinter( val )
+	regex = re.compile( "^yaal::hcore::HHashSet<.*>$" )
+	if regex.match( lookup_tag ):
+		return YaalHCoreHHashSetPrinter( val )
 	regex = re.compile( "^yaal::hcore::HNumber$" )
 	if regex.match( lookup_tag ):
 		return YaalHCoreHNumberPrinter( val )
@@ -111,33 +117,34 @@ class YaalHCoreHListPrinter:
 	def display_hint( self ):
 		return 'array'
 
+class BinaryTreeIterator( object ):
+	def __init__( self, owner, start, size ):
+		self._owner = owner
+		self.item = start
+		self.size = size
+		self.count = 0
+	def do_next( self, it ):
+		lastNode = it;
+		while it != 0:
+			if ( it['_right'] != 0 ) and ( it['_right'] != lastNode ):
+				it = it['_right']
+				while ( it['_left'] ):
+					it = it['_left']
+				break
+			else:
+				lastNode = it
+				it = it['_parent']
+				if ( it != 0 ) and ( lastNode == it['_left'] ):
+					break
+		return it
+
 class YaalHCoreHMapPrinter:
 	"Print a yaal::hcore::HMap"
 
-	class _iterator:
-		def __init__( self, owner, start, size ):
-			self._owner = owner
-			self.item = start
-			self.size = size
-			self.count = 0
+	class _iterator( BinaryTreeIterator ):
 
 		def __iter__(self):
 			return self
-
-		def do_next( self, it ):
-			lastNode = it;
-			while it != 0:
-				if ( it['_right'] != 0 ) and ( it['_right'] != lastNode ):
-					it = it['_right']
-					while ( it['_left'] ):
-						it = it['_left']
-					break
-				else:
-					lastNode = it
-					it = it['_parent']
-					if ( it != 0 ) and ( lastNode == it['_left'] ):
-						break
-			return it
 
 		def next(self):
 			if self.count == ( self.size * 2 ):
@@ -171,33 +178,71 @@ class YaalHCoreHMapPrinter:
 	def display_hint( self ):
 		return 'map'
 
-class YaalHCoreHHashMapPrinter:
-	"Print a yaal::hcore::HHashMap"
+class YaalHCoreHSetPrinter:
+	"Print a yaal::hcore::HSet"
 
-	class _iterator:
-		def __init__( self, owner, atom, index, size ):
-			self._owner = owner
-			self._atom = atom
-			self._index = index
-			self.size = size
-			self.count = 0
+	class _iterator( BinaryTreeIterator ):
 
 		def __iter__(self):
 			return self
 
-		def do_next( self ):
-			if self._atom != 0:
-				self._atom = self._atom['_next'];
-				if self._atom == 0:
-					self._index = self._index + 1;
+		def next(self):
+			if self.count == self.size:
+				raise StopIteration
+			count = self.count
+			nodetype = gdb.lookup_type( "yaal::hcore::HSBBSTree::HNode<%s>" % ( self._owner.type.template_argument( 0 ) ) ).pointer()
+			elt = self.item.cast( nodetype )['_key']
+			self.item = self.do_next( self.item )
+			self.count = self.count + 1
+			return ('[%d]' % count, elt)
+
+	def __init__( self, val ):
+		self.val = val
+
+	def begin( self ):
+		node = self.val['_engine']['_root']
+		while node['_left'] != 0:
+			node = node['_left']
+		return node
+
+	def children( self ):
+		return self._iterator( self.val, self.begin(), self.val['_engine']['_size'] )
+
+	def to_string( self ):
+		return ( "yaal::hcore::HSet of `%s' of length %d" % ( self.val.type.template_argument( 0 ), self.val['_engine']['_size'] ) )
+
+	def display_hint( self ):
+		return 'array'
+
+class HashContainterIterator( object ):
+	def __init__( self, owner, atom, index, size ):
+		self._owner = owner
+		self._atom = atom
+		self._index = index
+		self.size = size
+		self.count = 0
+
+	def do_next( self ):
+		if self._atom != 0:
+			self._atom = self._atom['_next'];
 			if self._atom == 0:
-				buckets = self._owner.buckets()
-				while ( self._index < self._owner.val['_engine']['_prime'] ) and ( buckets[ self._index ] == 0 ):
-					self._index = self._index + 1
-				if self._index < self._owner.val['_engine']['_prime']:
-					self._atom = buckets[ self._index ];
-			if self._atom == 0:
-				self._index = self._owner.val['_engine']['_prime'];
+				self._index = self._index + 1;
+		if self._atom == 0:
+			buckets = self._owner.buckets()
+			while ( self._index < self._owner.val['_engine']['_prime'] ) and ( buckets[ self._index ] == 0 ):
+				self._index = self._index + 1
+			if self._index < self._owner.val['_engine']['_prime']:
+				self._atom = buckets[ self._index ];
+		if self._atom == 0:
+			self._index = self._owner.val['_engine']['_prime'];
+
+
+class YaalHCoreHHashMapPrinter:
+	"Print a yaal::hcore::HHashMap"
+
+	class _iterator( HashContainterIterator ):
+		def __iter__(self):
+			return self
 
 		def next(self):
 			if self.count == ( self.size * 2 ):
@@ -234,6 +279,45 @@ class YaalHCoreHHashMapPrinter:
 
 	def display_hint( self ):
 		return 'map'
+
+class YaalHCoreHHashSetPrinter:
+	"Print a yaal::hcore::HHashSet"
+
+	class _iterator( HashContainterIterator ):
+		def __iter__(self):
+			return self
+
+		def next(self):
+			if self.count == self.size:
+				raise StopIteration
+			count = self.count
+			nodetype = self._owner.nodetype()
+			elt = self._atom.cast( nodetype )['_value']
+			self.do_next()
+			self.count = self.count + 1
+			return ('[%d]' % count, elt)
+
+	def __init__( self, val ):
+		self.val = val
+
+	def nodetype( self ):
+		nodetype = gdb.lookup_type( "yaal::hcore::HHashContainer::HAtom<%s>" % ( self.val.type.template_argument( 0 ) ) ).pointer()
+		return nodetype
+
+	def buckets( self ):
+		buckets = self.val['_engine']['_buckets']['_data']
+		return buckets.cast( self.nodetype().pointer() )
+
+	def children( self ):
+		it = self._iterator( self, 0, 0, self.val['_engine']['_size'] )
+		it.do_next()
+		return it
+
+	def to_string( self ):
+		return ( "yaal::hcore::HHashSet of `%s' of length %d" % ( self.val.type.template_argument( 0 ), self.val['_engine']['_size'] ) )
+
+	def display_hint( self ):
+		return 'array'
 
 class YaalHCoreHNumberPrinter:
 	"Print a yaal::hcore::HNumber"
