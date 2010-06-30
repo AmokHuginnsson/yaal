aa6b921ea6e5627534950271604a1242f1ef0d4f
Marcin Konarski <marcin.konarski@codestation.org>
2010-06-30 22:11:42 +0200

    preparation for CMake based client build.s

---
+++ _aux/client-config.hxx.cmake
@@ -0,0 +1,24 @@
+#ifndef @PROJECT_NAME_UC@_CONFIG_HXX_INCLUDED
+#define @PROJECT_NAME_UC@_CONFIG_HXX_INCLUDED 1
+#define PACKAGE_NAME "@PROJECT_NAME@"
+#define PACKAGE_VERSION "0.0.0"
+#define PACKAGE_STRING PACKAGE_NAME" "PACKAGE_VERSION
+#ifndef __GNUC__
+#define __attribute__( x ) /**/
+#endif /* not __GNUC__ */
+#ifdef __MSVCXX__
+#define WIN32_LEAN_AND_MEAN 1
+#define VC_EXTRALEAN 1
+#pragma warning( disable : 4068 )
+#pragma warning( disable : 4180 )
+#pragma warning( disable : 4250 )
+#pragma warning( disable : 4351 )
+#pragma warning( disable : 4355 )
+#pragma warning( disable : 4646 )
+#pragma warning( disable : 4996 )
+#undef __PRETTY_FUNCTION__
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#define HAVE_PRETTY_FUNCTION 1
+#include "cleanup.hxx"
+#endif /* __MSVCXX__ */
+#endif /* not @PROJECT_NAME_UC@_CONFIG_HXX_INCLUDED */
