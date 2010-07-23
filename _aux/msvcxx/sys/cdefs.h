25c42a895ff63b457ddc4b01c0946b0e9889d882
Marcin Konarski <marcin.konarski@codestation.org>
2010-07-23 17:02:23 +0200

    msvcxx compat: forgotten file..

---
+++ _aux/msvcxx/sys/cdefs.h
@@ -0,0 +1,9 @@
+#ifndef YAAL_MSVCXX_SYS_CDEFS_H_INCLUDED
+#define YAAL_MSVCXX_SYS_CDEFS_H_INCLUDED 1
+
+#include <glibc/sys/cdefs.h>
+#undef __inline
+#undef __volatile
+
+#endif /* not YAAL_MSVCXX_SYS_CDEFS_H_INCLUDED */
+
