c06981e9c7b534602127530691245003df0e441e
Marcin Konarski <marcin.konarski@codestation.org>
2010-08-30 19:30:18 +0200

    Fixed bug in HDes implementation on 64bit platform (idiotic typedefs).

--- tools/hdes.cxx
+++ tools/hdes.cxx
@@ -40,9 +40,6 @@ namespace yaal
 namespace tools
 {
 
-typedef int long unsigned u32_t;
-typedef int short unsigned u16_t;
-
 /*! \brief The DES algorithm helper data tables.
  */
 namespace des
@@ -157,7 +154,7 @@ void HDes::_des( u8_t* block_, int action_, int part_ )
 	src.use( bufT, DES::BLOCK_SIZE * DES::BITS_IN_BYTE );
 	rowBmp.use( &row, sizeof ( row ) * DES::BITS_IN_BYTE );
 	colBmp.use( &col, sizeof ( col ) * DES::BITS_IN_BYTE );
-	for ( cycle = 0; cycle < DES::IKEYS_COUNT; cycle ++ )
+	for ( cycle = 0; cycle < DES::IKEYS_COUNT; ++ cycle )
 		{
 		reinterpret_cast<u32_t*>( buf )[ 0 ] = 0;
 		if ( action_ )
@@ -258,10 +255,11 @@ const u8_t _permutationOfCompresion_[] =
 /* extension permutation */
 const u8_t _permutationOfExpanding_[] =
 		{
-				31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8,
-				7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16,
-				15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24,
-				23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0
+				31,  0,  1,  2,  3,  4,  3,  4,  5,  6,
+				 7,  8,  7,  8,  9, 10, 11, 12, 11, 12,
+				13, 14, 15, 16, 15, 16, 17, 18, 19, 20,
+				19, 20, 21, 22, 23, 24, 23, 24, 25, 26,
+				27, 28, 27, 28, 29, 30, 31, 0
 		};
 		
 /* P-block permutation */
