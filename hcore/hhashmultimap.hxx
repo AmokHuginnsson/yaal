ad738488498e12c130486c86871068bf985e4c25
Marcin Konarski <marcin.konarski@codestation.org>
2010-12-03 11:54:11 +0100

    Compilation fix.

--- hcore/hhashmultimap.hxx
+++ hcore/hhashmultimap.hxx
@@ -101,7 +101,7 @@ public:
 		{
 		M_PROLOG
 		int long sizeAcc( 0 );
-		for ( typename hashmultimap_engine_t::const_iterator it( _engine.begin() ), endIt( _engine.endIt() ); it != endIt; ++ it )
+		for ( typename hashmultimap_engine_t::const_iterator it( _engine.begin() ), endIt( _engine.end() ); it != endIt; ++ it )
 			sizeAcc += it->second->get_size();
 		return ( sizeAcc );
 		M_EPILOG
