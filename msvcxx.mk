9bd42a5c338af09f29ee60f6391a00b56f02c984
Marcin Konarski <marcin.konarski@codestation.org>
2011-03-20 22:15:25 +0100

    cmd line build on windows xp cygwin

--- msvcxx.mk
+++ msvcxx.mk
@@ -1,18 +1,27 @@
 # a makefile to build using Microsoft Visual Studio.
 
-.PHONY: all install clean purge mrproper
+.PHONY: all debug install clean purge mrproper
 
 PROJECT_NAME=$(notdir $(CURDIR))
 
-all: $(PROJECT_NAME).sln
-	/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild /showenv /M1 $(PROJECT_NAME).sln "Debug|Win32"
+VCBUILD=$(wildcard /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild.exe)$(wildcard /cygdrive/c/Program\ Files/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild.exe)
+BUILD_ARTIFACT=ZERO_CHECK.dir/Debug/BuildLog.htm
+
+all: debug
+
+debug: $(BUILD_ARTIFACT)
+
+$(BUILD_ARTIFACT): $(PROJECT_NAME).sln
+	"$(VCBUILD)" /showenv /M1 $(PROJECT_NAME).sln "Debug|Win32" ; \
+	test -f $(@)
+
+$(PROJECT_NAME).sln: ./configure.js ./local.js
+	cscript ./configure.js ; \
+	test -f $(@)
 
 install: all
-	/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild /showenv /M1 INSTALL.vcproj "Debug|Win32"
+	"$(VCBUILD)" /showenv /M1 INSTALL.vcproj "Debug|Win32"
 
 purge clean mrproper:
 	$(MAKE) purge
-
-$(PROJECT_NAME).sln: ./configure.js ./local.js
-	cscript ./configure.js
 
