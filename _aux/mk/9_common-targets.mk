157f8e80e9fcd33074f15ed12ad2822de0de4bea
Marcin Konarski <marcin.konarski@codestation.org>
2011-05-26 17:14:11 +0200

    console build for both VS2008 and VS2010.

--- _aux/mk/9_common-targets.mk
+++ _aux/mk/9_common-targets.mk
@@ -43,7 +43,8 @@ purge: mrproper
 		config.h config.h.in yaalrc config.log dirs.d doc/html \
 		CMakeFiles CMakeCache.txt cmake_install.cmake install_manifest.txt \
 		tags GPATH GRTAGS GSYMS GTAGS make.log *.vcproj.* *.vcproj *.vcxproj.* *.vcxproj \
-		*.sln *.suo *.ncb *.sdf *.dir _UpgradeReport_Files UpgradeLog.XML debug release *.so && \
+		*.sln *.suo *.ncb *.sdf *.dir _UpgradeReport_Files UpgradeLog.XML debug release *.so \
+		ipch Win32 *.sln.cache build.stamp && \
 	if [ "x${OSTYPE}" != "xcygwin" ] ; then /bin/rm -f Makefile ; fi
 
 bin:
--- msvcxx.mk
+++ msvcxx.mk
@@ -5,14 +5,25 @@
 PROJECT_NAME=$(notdir $(CURDIR))
 
 VCBUILD=$(wildcard /cygdrive/c/Program\ Files\ (x86)/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild.exe)$(wildcard /cygdrive/c/Program\ Files/Microsoft\ Visual\ Studio\ 9.0/VC/vcpackages/vcbuild.exe)
-BUILD_ARTIFACT=ZERO_CHECK.dir/Debug/BuildLog.htm
+MSBUILD=$(wildcard /cygdrive/c/Windows/Microsoft.NET/Framework/v4*/MSBuild.exe)
+BUILD_ARTIFACT=build.stamp
 
 all: debug
 
 debug: $(BUILD_ARTIFACT)
 
 $(BUILD_ARTIFACT): $(wildcard */*.cxx) $(wildcard */*.hxx)  $(wildcard */*/*.cxx) $(wildcard */*/*.hxx) $(PROJECT_NAME).sln
-	"$(VCBUILD)" /showenv /M1 $(PROJECT_NAME).sln "Debug|Win32" ; \
+	@export VS_VER="x`awk '/# Visual Studio /{print $$4}' $(PROJECT_NAME).sln`" ; \
+	if [ "$${VS_VER}" = "x2008" ] ; then \
+		"$(VCBUILD)" /showenv /M1 $(PROJECT_NAME).sln "Debug|Win32" ; \
+	else \
+		if [ "$${VS_VER}" = "x2010" ] ; then \
+			"$(MSBUILD)" $(PROJECT_NAME).sln /nologo /m:4 /t:Build /clp:NoItemAndPropertyList /p:Configuration=Debug /p:Platform=Win32 ; \
+		else \
+			echo "Cannot guess VS version!" && false ; \
+		fi \
+	fi && \
+	test -f build/debug/hdata-d.dll -o -f build/release/hdata-d.dll -o -f build/debug/$(PROJECT_NAME)/1exec.exe -o -f build/release/$(PROJECT_NAME)/1exec.exe && touch $(BUILD_ARTIFACT) && \
 	test -f $(@)
 
 $(PROJECT_NAME).sln: ./configure.js ./local.js
@@ -20,8 +31,30 @@ $(PROJECT_NAME).sln: ./configure.js ./local.js
 	test -f $(@)
 
 install: all
-	"$(VCBUILD)" /showenv /M1 INSTALL.vcproj "Debug|Win32"
+	@export VS_VER="x`awk '/# Visual Studio /{print $$4}' $(PROJECT_NAME).sln`" ; \
+	if [ "$${VS_VER}" = "x2008" ] ; then \
+		"$(VCBUILD)" /showenv /M1 INSTALL.vcproj "Debug|Win32" ; \
+	else \
+		if [ "$${VS_VER}" = "x2010" ] ; then \
+			"$(MSBUILD)" INSTALL.vc*proj /nologo /m:1 /t:Build /clp:NoItemAndPropertyList /p:Configuration=Debug /p:Platform=Win32 ; \
+		else \
+			echo "Cannot guess VS version!" && false ; \
+		fi \
+	fi
 
-purge clean mrproper:
+clean: $(PROJECT_NAME).sln
+	@export VS_VER="x`awk '/# Visual Studio /{print $$4}' $(PROJECT_NAME).sln`" ; \
+	if [ "$${VS_VER}" = "x2008" ] ; then \
+		"$(VCBUILD)" /showenv /M1 $(PROJECT_NAME).sln /clean "Debug|Win32" ; \
+	else \
+		if [ "$${VS_VER}" = "x2010" ] ; then \
+			"$(MSBUILD)" $(PROJECT_NAME).sln /nologo /t:Clean /v:normal /p:Configuration=Debug /p:Platform=Win32 ; \
+		else \
+			echo "Cannot guess VS version!" && false ; \
+		fi \
+	fi ; \
+	/bin/rm -f $(BUILD_ARTIFACT)
+
+purge mrproper:
 	$(MAKE) purge
 
