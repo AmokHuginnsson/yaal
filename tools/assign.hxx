aa9f3360a87634faee0a7f6edbfa878fd02dac5e
Marcin Konarski <marcin.konarski@codestation.org>
2012-01-04 09:36:25 +0100

    additional assign helper.

--- tools/assign.hxx
+++ tools/assign.hxx
@@ -125,6 +125,177 @@ HAssignPair<T, S> sequence( T const& first_, S const& second_ ) {
 	M_EPILOG
 }
 
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr( 1, a0_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 2 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 3 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_, T const& a3_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 4 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_, T const& a3_, T const& a4_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 5 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	arr.push_back( a4_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_,
+		T const& a3_, T const& a4_, T const& a5_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 6 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	arr.push_back( a4_ );
+	arr.push_back( a5_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_,
+		T const& a3_, T const& a4_, T const& a5_, T const& a6_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 7 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	arr.push_back( a4_ );
+	arr.push_back( a5_ );
+	arr.push_back( a6_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_,
+		T const& a3_, T const& a4_, T const& a5_, T const& a6_, T const& a7_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 8 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	arr.push_back( a4_ );
+	arr.push_back( a5_ );
+	arr.push_back( a6_ );
+	arr.push_back( a7_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_,
+		T const& a3_, T const& a4_, T const& a5_, T const& a6_, T const& a7_, T const& a8_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 9 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	arr.push_back( a4_ );
+	arr.push_back( a5_ );
+	arr.push_back( a6_ );
+	arr.push_back( a7_ );
+	arr.push_back( a8_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_,
+		T const& a3_, T const& a4_, T const& a5_, T const& a6_, T const& a7_,
+		T const& a8_, T const& a9_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 10 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	arr.push_back( a4_ );
+	arr.push_back( a5_ );
+	arr.push_back( a6_ );
+	arr.push_back( a7_ );
+	arr.push_back( a8_ );
+	arr.push_back( a9_ );
+	return ( arr );
+	M_EPILOG
+}
+
+template<typename T>
+yaal::hcore::HArray<T> array( T const& a0_, T const& a1_, T const& a2_,
+		T const& a3_, T const& a4_, T const& a5_, T const& a6_, T const& a7_,
+		T const& a8_, T const& a9_, T const& a10_ ) {
+	M_PROLOG
+	yaal::hcore::HArray<T> arr;
+	arr.reserve( 11 );
+	arr.push_back( a0_ );
+	arr.push_back( a1_ );
+	arr.push_back( a2_ );
+	arr.push_back( a3_ );
+	arr.push_back( a4_ );
+	arr.push_back( a5_ );
+	arr.push_back( a6_ );
+	arr.push_back( a7_ );
+	arr.push_back( a8_ );
+	arr.push_back( a9_ );
+	arr.push_back( a10_ );
+	return ( arr );
+	M_EPILOG
+}
+
 }
 
 }
