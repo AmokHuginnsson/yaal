b29d7d781ecc1bf5dd2bf1b840d3e4d8d0a956e6
Marcin Konarski <marcin.konarski@codestation.org>
2014-08-05 14:58:25 +0200

    hconsole: Added HWindowFactory class to support loading HWindows from XML resource file.

---
+++ hconsole/hwindowfactory.cxx
@@ -0,0 +1,124 @@
+/*
+---           `yaal' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+	hwindowfactory.cxx - this file is integral part of `yaal' project.
+
+  i.  You may not make any changes in Copyright information.
+  ii. You must attach Copyright information to any part of every copy
+      of this software.
+
+Copyright:
+
+ You can use this software free of charge and you can redistribute its binary
+ package freely but:
+  1. You are not allowed to use any part of sources of this software.
+  2. You are not allowed to redistribute any part of sources of this software.
+  3. You are not allowed to reverse engineer this software.
+  4. If you want to distribute a binary package of this software you cannot
+     demand any fees for it. You cannot even demand
+     a return of cost of the media or distribution (CD for example).
+  5. You cannot involve this software in any commercial activity (for example
+     as a free add-on to paid software or newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <libintl.h>
+
+#include "hcore/base.hxx"
+M_VCSID( "$Id: " __ID__ " $" )
+M_VCSID( "$Id: " __TID__ " $" )
+#include "hwindowfactory.hxx"
+#include "hcore/foreach.hxx"
+
+using namespace yaal;
+using namespace yaal::hcore;
+using namespace yaal::tools;
+
+namespace yaal {
+
+namespace hconsole {
+
+void HWindowFactory::register_widget_creator( HString const& name_, HWindowCreatorInterface::ptr_t creator_ ) {
+	M_PROLOG
+	creators_t::iterator it = _creators.find( name_ );
+	if ( it != _creators.end() ) {
+		M_THROW( _( "Window already registered" ), errno );
+	}
+	_creators[ name_ ] = creator_;
+	return;
+	M_EPILOG
+}
+
+HWindow::ptr_t HWindowFactory::create_window( HTUIProcess* tuiProcess_, yaal::tools::HXml::HConstNodeProxy const& node_ ) {
+	M_PROLOG
+	HWindow::ptr_t widget;
+	creators_t::iterator it = _creators.find( node_.get_name() );
+	if ( it != _creators.end() ) {
+		widget = it->second->new_instance( tuiProcess_, node_ );
+	}
+	return ( widget );
+	M_EPILOG
+}
+
+bool HWindowFactory::is_type_valid( yaal::hcore::HString const& type_ ) {
+	M_PROLOG
+	return ( _creators.find( type_ ) != _creators.end() );
+	M_EPILOG
+}
+
+HWindowFactory::creators_t::iterator HWindowFactory::begin( void ) {
+	return ( _creators.begin() );
+}
+
+HWindowFactory::creators_t::iterator HWindowFactory::end( void ) {
+	return ( _creators.end() );
+}
+
+int HWindowFactory::life_time( int lifeTime_ ) {
+	return ( lifeTime_ );
+}
+
+void HWindowFactory::initialize_globals( void ) {
+	M_PROLOG
+	for ( creators_t::iterator it = _creators.begin(); it != _creators.end(); ++ it ) {
+		it->second->initialize_globals();
+	}
+	return;
+	M_EPILOG
+}
+
+void HWindowFactory::cleanup_globals( void ) {
+	M_PROLOG
+	for ( creators_t::iterator it = _creators.begin(); it != _creators.end(); ++ it ) {
+		it->second->cleanup_globals();
+	}
+	return;
+	M_EPILOG
+}
+
+void HWindowCreatorInterface::initialize_globals( void ) {
+	M_PROLOG
+	do_initialize_globals();
+	return;
+	M_EPILOG
+}
+
+void HWindowCreatorInterface::cleanup_globals( void ) {
+	M_PROLOG
+	do_cleanup_globals();
+	return;
+	M_EPILOG
+}
+
+HWindow::ptr_t HWindowCreatorInterface::new_instance( HTUIProcess* tuiProcess_, yaal::tools::HXml::HConstNodeProxy const& node_ ) {
+	M_PROLOG
+	return ( do_new_instance( tuiProcess_, node_ ) );
+	M_EPILOG
+}
+
+}
+
+}
+
---
+++ hconsole/hwindowfactory.hxx
@@ -0,0 +1,83 @@
+/*
+---           `yaal' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+	hwindowfactory.hxx - this file is integral part of `yaal' project.
+
+  i.  You may not make any changes in Copyright information.
+  ii. You must attach Copyright information to any part of every copy
+      of this software.
+
+Copyright:
+
+ You can use this software free of charge and you can redistribute its binary
+ package freely but:
+  1. You are not allowed to use any part of sources of this software.
+  2. You are not allowed to redistribute any part of sources of this software.
+  3. You are not allowed to reverse engineer this software.
+  4. If you want to distribute a binary package of this software you cannot
+     demand any fees for it. You cannot even demand
+     a return of cost of the media or distribution (CD for example).
+  5. You cannot involve this software in any commercial activity (for example
+     as a free add-on to paid software or newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+/*! \file hconsole/hwindowfactory.hxx
+ * \brief Declaration of HWindowFactory class.
+ */
+
+#ifndef YAAL_HCONSOLE_HWINDOWFACTORY_HXX_INCLUDED
+#define YAAL_HCONSOLE_HWINDOWFACTORY_HXX_INCLUDED 1
+
+#include "hcore/hsingleton.hxx"
+#include "tools/hxml.hxx"
+#include "hconsole/hwindow.hxx"
+
+namespace yaal {
+
+namespace hconsole {
+
+class HWindowCreatorInterface {
+protected:
+	virtual void do_initialize_globals( void ) {};
+	virtual void do_cleanup_globals( void ) {};
+	virtual HWindow::ptr_t do_new_instance( HTUIProcess*, yaal::tools::HXml::HConstNodeProxy const& ) = 0;
+public:
+	virtual ~HWindowCreatorInterface( void ) {}
+	void initialize_globals( void );
+	void cleanup_globals( void );
+	HWindow::ptr_t new_instance( HTUIProcess*, yaal::tools::HXml::HConstNodeProxy const& );
+	typedef yaal::hcore::HPointer<HWindowCreatorInterface> ptr_t;
+};
+
+class HWindowFactory : public yaal::hcore::HSingleton<HWindowFactory> {
+public:
+	typedef HWindowFactory this_type;
+	typedef yaal::hcore::HMap<yaal::hcore::HString, HWindowCreatorInterface::ptr_t> creators_t;
+private:
+	creators_t _creators;
+public:
+	void register_widget_creator( yaal::hcore::HString const&, HWindowCreatorInterface::ptr_t );
+	HWindow::ptr_t create_window( HTUIProcess*, yaal::tools::HXml::HConstNodeProxy const& );
+	bool is_type_valid( yaal::hcore::HString const& );
+	creators_t::iterator begin( void );
+	creators_t::iterator end( void );
+	void initialize_globals( void );
+	void cleanup_globals( void );
+private:
+	HWindowFactory( void ) : _creators() {}
+	~HWindowFactory( void ) {}
+	static int life_time( int );
+	friend class yaal::hcore::HSingleton<HWindowFactory>;
+	friend class yaal::hcore::HDestructor<HWindowFactory>;
+};
+
+typedef yaal::hcore::HSingleton<HWindowFactory> HWindowFactoryInstance;
+
+}
+
+}
+
+#endif /* #ifndef YAAL_HCONSOLE_HWINDOWFACTORY_HXX_INCLUDED */
+
