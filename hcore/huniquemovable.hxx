6c548fd0e97c3a67aba79d439bf9e302aaecd8cd
Marcin Konarski <marcin.konarski@codestation.org>
2014-10-25 22:15:50 +0200

    HUniqueMovable<>: Use parameter pack and perfect forwarding instead of copy-and-paste. Implement true move semantics.

--- hcore/huniquemovable.hxx
+++ hcore/huniquemovable.hxx
@@ -33,23 +33,11 @@ Copyright:
 #include <cstring>
 
 #include "hcore/hexception.hxx"
+#include "hcore/algorithm_low.hxx"
 
 namespace yaal {
 
 namespace hcore {
-
-/*! \brief Provide reference semantics for HUniqueMovable<>.
- */
-template<typename tType>
-struct HUniqueMovableRef {
-	char _mem[sizeof ( tType )];
-	bool _owner;
-	explicit HUniqueMovableRef( tType* obj_, bool owner_ )
-		: _mem(), _owner( owner_ ) {
-		if ( owner_ )
-			::memcpy( static_cast<void*>( _mem ), static_cast<void*>( obj_ ), sizeof ( tType ) );
-	}
-};
 
 /*! \brief Keep memory movable object on stack and ensure its uniqeness.
  */
@@ -61,114 +49,23 @@ private:
 	char _mem[sizeof ( value_type )];
 	bool _owner;
 public:
-	HUniqueMovable( void )
+	template<typename... arg_t>
+	HUniqueMovable( arg_t&&... arg_ )
 		: _mem(), _owner( true ) {
 		M_PROLOG
-		new ( _mem ) value_type();
+		new ( _mem ) value_type( yaal::forward<arg_t>( arg_ )... );
 		M_EPILOG
 	}
-	template<typename a0_t>
-	HUniqueMovable( a0_t a0_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t,
-		typename a4_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_, a4_t a4_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_, a4_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t,
-		typename a4_t, typename a5_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_, a4_t a4_, a5_t a5_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_, a4_, a5_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t,
-		typename a4_t, typename a5_t, typename a6_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_, a4_t a4_, a5_t a5_, a6_t a6_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_, a4_, a5_, a6_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t,
-		typename a4_t, typename a5_t, typename a6_t, typename a7_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_, a4_t a4_, a5_t a5_, a6_t a6_, a7_t a7_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_, a4_, a5_, a6_, a7_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t,
-		typename a4_t, typename a5_t, typename a6_t, typename a7_t,
-		typename a8_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_, a4_t a4_, a5_t a5_, a6_t a6_, a7_t a7_, a8_t a8_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_, a4_, a5_, a6_, a7_, a8_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t,
-		typename a4_t, typename a5_t, typename a6_t, typename a7_t,
-		typename a8_t, typename a9_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_, a4_t a4_, a5_t a5_, a6_t a6_, a7_t a7_, a8_t a8_, a9_t a9_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_, a4_, a5_, a6_, a7_, a8_, a9_ );
-		M_EPILOG
-	}
-	template<typename a0_t, typename a1_t, typename a2_t, typename a3_t,
-		typename a4_t, typename a5_t, typename a6_t, typename a7_t,
-		typename a8_t, typename a9_t, typename a10_t>
-	HUniqueMovable( a0_t a0_, a1_t a1_, a2_t a2_, a3_t a3_, a4_t a4_, a5_t a5_, a6_t a6_, a7_t a7_, a8_t a8_, a9_t a9_, a10_t a10_ )
-		: _mem(), _owner( true ) {
-		M_PROLOG
-		new ( _mem ) value_type( a0_, a1_, a2_, a3_, a4_, a5_, a6_, a7_, a8_, a9_, a10_ );
-		M_EPILOG
-	}
-	HUniqueMovable( HUniqueMovable& um_ )
+	HUniqueMovable( HUniqueMovable&& um_ )
 		: _mem(), _owner( true ) {
 		::memcpy( _mem, um_._mem, sizeof ( value_type ) );
 		um_._owner = false;
-	}
-	HUniqueMovable( HUniqueMovableRef<tType> um_ )
-		: _mem(), _owner( um_._owner ) {
-		if ( um_._owner )
-			::memcpy( _mem, um_._mem, sizeof ( value_type ) );
 	}
 	~HUniqueMovable( void ) {
 		reset();
 	}
 /* cppcheck-suppress operatorEqVarError */
-	HUniqueMovable& operator = ( HUniqueMovable& um_ ) {
+	HUniqueMovable& operator = ( HUniqueMovable&& um_ ) {
 		M_PROLOG
 		if ( &um_ != this ) {
 			reset();
@@ -177,16 +74,6 @@ public:
 				um_._owner = false;
 				_owner = true;
 			}
-		}
-		return ( *this );
-		M_EPILOG
-	}
-	HUniqueMovable& operator = ( HUniqueMovableRef<tType> um_ ) {
-		M_PROLOG
-		reset();
-		if ( um_._owner ) {
-			::memcpy( _mem, um_._mem, sizeof ( value_type ) );
-			_owner = true;
 		}
 		return ( *this );
 		M_EPILOG
@@ -213,14 +100,12 @@ public:
 		M_ASSERT( _owner );
 		return ( static_cast<value_type*>( static_cast<void*>( _mem ) ) );
 	}
-	operator HUniqueMovableRef<value_type>( void ) {
-		HUniqueMovableRef<value_type> ref( static_cast<value_type*>( static_cast<void*>( _mem ) ), _owner );
-		_owner = false;
-		return ( ref );
-	}
 	bool has_ownership( void ) const {
 		return ( _owner );
 	}
+private:
+	HUniqueMovable( HUniqueMovable const& ) = delete;
+	HUniqueMovable& operator = ( HUniqueMovable const& ) = delete;
 };
 
 }
