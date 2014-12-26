f669c8aeb50ef4ba6d3126120bff4b20819d76c9
Marcin Konarski <marcin.konarski@codestation.org>
2014-12-26 02:43:28 +0100

    Style.

--- hcore/hhashcontainer.cxx
+++ hcore/hhashcontainer.cxx
@@ -1,7 +1,7 @@
 /*
 ---           `yaal' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
 
-	hhashcontainer.cxx - this file is integral part of `yaal' project.
+  hhashcontainer.cxx - this file is integral part of `yaal' project.
 
   i.  You may not make any changes in Copyright information.
   ii. You must attach Copyright information to any part of every copy
@@ -35,12 +35,12 @@ namespace yaal {
 namespace hcore {
 
 int long const x_tag_g_pulPrimes[ 32 ] = {
-	1,					3,					7,					13,					31,					61,
-	127,				251,				509,				1021,				2039,				4093,
-	8191,				16381,			32749,			65521,			131071,			262139,
-	524287,			1048573,		2097143,		4194301,		8388593,		16777213,
-	33554393,		67108859,		134217689,	268435399,	536870909,	1073741789,
-	2147483647,	0
+	1,          3,        7,         13,        31,        61,
+	127,        251,      509,       1021,      2039,      4093,
+	8191,       16381,    32749,     65521,     131071,    262139,
+	524287,     1048573,  2097143,   4194301,   8388593,   16777213,
+	33554393,   67108859, 134217689, 268435399, 536870909, 1073741789,
+	2147483647, 0
 }, * const _primes_ = x_tag_g_pulPrimes;
 
 template<>
