f17e53e8cec05aa083307907d6cf0c78c197a432
Marcin Konarski <marcin.konarski@codestation.org>
2015-04-27 05:09:26 +0200

    HHuginn: Refactor HPreprocessor into separate implementation file.

---
+++ tools/huginn/preprocessor.cxx
@@ -0,0 +1,201 @@
+/*
+---           `yaal' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+  preprocessor.cxx - this file is integral part of `yaal' project.
+
+  i.  You may not make any changes in Copyright information.
+  ii. You must attach Copyright information to any part of every copy
+      of this software.
+
+Copyright:
+
+ You can use this software free of charge and you can redistribute its binary
+ package freely but:
+  1. You are not allowed to use any part of sources of this software.
+  2. You are not allowed to redistribute any part of sources of this software.
+  3. You are not allowed to reverse engineer this software.
+  4. If you want to distribute a binary package of this software you cannot
+     demand any fees for it. You cannot even demand
+     a return of cost of the media or distribution (CD for example).
+  5. You cannot involve this software in any commercial activity (for example
+     as a free add-on to paid software or newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <cstring>
+
+#include "hcore/base.hxx"
+M_VCSID( "$Id: " __ID__ " $" )
+M_VCSID( "$Id: " __TID__ " $" )
+#include "preprocessor.hxx"
+
+using namespace yaal;
+using namespace yaal::hcore;
+
+namespace yaal {
+
+namespace tools {
+
+namespace huginn {
+
+/*
+ * These cannot be declared inside HPrepocessor body because HIterator is not fully declared there.
+ */
+HPrepocessor::HIterator HPrepocessor::begin( void ) const {
+	int len( static_cast<int>( _end - _beg ) );
+	char const hashBang[] = "#!";
+	int const hashBangLen( static_cast<int>( sizeof ( hashBang ) - 1 ) );
+	int offset( 0 );
+	if ( ( len >= hashBangLen ) && ! strncmp( _beg, hashBang, hashBangLen ) ) {
+		char const* nl( static_cast<char const*>( ::memchr( _beg, NEWLINE, static_cast<size_t>( len ) ) ) );
+		if ( nl ) {
+			offset = static_cast<int>( nl + 1 - _beg );
+		}
+	}
+	return ( HIterator( this, _beg + offset ) );
+}
+
+HPrepocessor::HIterator HPrepocessor::end( void ) const {
+	return ( HIterator( this, _end ) );
+}
+
+yaal::hcore::HString::const_iterator HPrepocessor::HIterator::skip_comment( yaal::hcore::HString::const_iterator pos_ ) {
+	yaal::hcore::HString::const_iterator pos( pos_ );
+	yaal::hcore::HString::const_iterator goodPos( pos_ );
+	while ( true ) {
+		if ( *pos == COMMENT_START_CHAR1 ) {
+			/* we are possibly in comment, lets check next character */
+			++ pos;
+			if ( pos == _owner->_end ) {
+				/* We are at end of source, we should not update _cur */
+				pos = goodPos;
+				break;
+			} else if ( *pos == COMMENT_START_CHAR2 ) {
+#if 0
+				we are in /* ... */ comment
+#endif /* #if 0 */
+				++ pos;
+				while ( pos != _owner->_end ) {
+					/* we look for end of comment */
+					if ( *pos == COMMENT_STOP_CHAR1 ) {
+						/* possibly we found first character of end of comment */
+						++ pos;
+						if ( pos != _owner->_end ) {
+							if ( *pos == COMMENT_STOP_CHAR2 ) {
+								/* we found end of comment. */
+								++ pos;
+								break;
+							}
+						} else {
+							/* unfinished comment */
+							break;
+						}
+					}
+					++ pos;
+				}
+				if ( pos == _owner->_end ) {
+					break;
+				}
+				goodPos = pos;
+				continue;
+			} else if ( *pos == COMMENT_START_CHAR2ALT ) {
+				/* We are in single line comment. */
+				++ pos;
+				while ( pos != _owner->_end ) {
+					if ( *pos == NEWLINE ) {
+						break;
+					}
+					++ pos;
+				}
+				if ( pos == _owner->_end ) {
+					break;
+				}
+				goodPos = pos;
+				continue;
+			} else {
+				pos = goodPos;
+			}
+		}
+		break;
+	}
+	return ( pos );
+}
+
+void HPrepocessor::HIterator::make_readable( void ) {
+	/*
+	 * Make_readable always starts in NORMAL state or in one of the QUOTEs,
+	 * and always ends in NORMAL state or in one of the QUOTEs.
+	 */
+	yaal::hcore::HString::const_iterator pos( _cur );
+	if ( _state == STATE::NORMAL ) {
+		pos = skip_comment( pos );
+		if ( *pos == SINGLE_QUOTE ) {
+			_state = STATE::IN_SINGLE_QUOTE;
+		} else if ( *pos == DOUBLE_QUOTE ) {
+			_state = STATE::IN_DOUBLE_QUOTE;
+		}
+	} else {
+		/* We are in quote state. */
+		if ( _escape ) {
+			_escape = false;
+		} else if ( *pos == ESCAPE ) {
+			_escape = true;
+		} else {
+			if ( ( ( _state == STATE::IN_SINGLE_QUOTE ) && ( *pos == SINGLE_QUOTE ) )
+					|| ( ( _state == STATE::IN_DOUBLE_QUOTE ) && ( *pos == DOUBLE_QUOTE ) ) ) {
+				pos = try_skip_whitespace( pos, *pos );
+			}
+		}
+	}
+	_cur = pos;
+	return;
+}
+
+yaal::hcore::HString::const_iterator HPrepocessor::HIterator::skip_whitespace( yaal::hcore::HString::const_iterator pos_ ) {
+	yaal::hcore::HString::const_iterator pos( pos_ );
+	while ( pos != _owner->_end ) {
+		if ( ! is_whitespace( *pos ) ) {
+			break;
+		}
+		++ pos;
+	}
+	return ( pos );
+}
+
+yaal::hcore::HString::const_iterator HPrepocessor::HIterator::try_skip_whitespace( yaal::hcore::HString::const_iterator pos_, char quote_ ) {
+	yaal::hcore::HString::const_iterator pos( pos_ );
+	/* We are at closing quote character. */
+	++ pos;
+	while ( pos != _owner->_end ) {
+		yaal::hcore::HString::const_iterator orig( pos );
+		pos = skip_whitespace( pos );
+		if ( pos == _owner->_end ) {
+			pos = pos_;
+			break;
+		}
+		pos = skip_comment( pos );
+		if ( pos == _owner->_end ) {
+			pos = pos_;
+			break;
+		}
+		if ( pos == orig ) {
+			break;
+		}
+	}
+	if ( ( pos != _owner->_end ) && ( *pos == quote_ ) ) {
+		++ pos;
+	} else {
+		pos = pos_;
+		_state = STATE::NORMAL;
+	}
+	return ( pos );
+}
+
+}
+
+}
+
+}
+
---
+++ tools/huginn/preprocessor.hxx
@@ -0,0 +1,157 @@
+/*
+---           `yaal' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+  preprocessor.hxx - this file is integral part of `yaal' project.
+
+  i.  You may not make any changes in Copyright information.
+  ii. You must attach Copyright information to any part of every copy
+      of this software.
+
+Copyright:
+
+ You can use this software free of charge and you can redistribute its binary
+ package freely but:
+  1. You are not allowed to use any part of sources of this software.
+  2. You are not allowed to redistribute any part of sources of this software.
+  3. You are not allowed to reverse engineer this software.
+  4. If you want to distribute a binary package of this software you cannot
+     demand any fees for it. You cannot even demand
+     a return of cost of the media or distribution (CD for example).
+  5. You cannot involve this software in any commercial activity (for example
+     as a free add-on to paid software or newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+/* YAAL_PRIVATE_IMPLEMENTATION_DETAIL */
+
+#ifndef YAAL_TOOLS_HUGINN_PREPROCESSOR_HXX_INCLUDED
+#define YAAL_TOOLS_HUGINN_PREPROCESSOR_HXX_INCLUDED 1
+
+#include "hcore/hstring.hxx"
+#include "hcore/iterator.hxx"
+
+namespace yaal {
+
+namespace tools {
+
+namespace huginn {
+
+namespace {
+static char const NEWLINE = '\n';
+}
+
+class HPrepocessor {
+public:
+	typedef HPrepocessor this_type;
+	class HIterator;
+private:
+	yaal::hcore::HString::const_iterator _beg;
+	yaal::hcore::HString::const_iterator _end;
+public:
+	HPrepocessor( yaal::hcore::HString::const_iterator first_, yaal::hcore::HString::const_iterator last_ )
+		: _beg( first_ ), _end( last_ ) {
+		return;
+	}
+	HIterator begin( void ) const;
+	HIterator end( void ) const;
+};
+
+class HPrepocessor::HIterator : public yaal::hcore::iterator_interface<char const, yaal::hcore::iterator_category::forward> {
+public:
+	typedef HIterator this_type;
+	typedef yaal::hcore::iterator_interface<char const, yaal::hcore::iterator_category::forward> base_type;
+private:
+	static char const COMMENT_START_CHAR1 = '/';
+	static char const COMMENT_START_CHAR2 = '*';
+	static char const COMMENT_START_CHAR2ALT = '/';
+	static char const COMMENT_STOP_CHAR1 = '*';
+	static char const COMMENT_STOP_CHAR2 = '/';
+	static char const ESCAPE = '\\';
+	static char const DOUBLE_QUOTE = '"';
+	static char const SINGLE_QUOTE = '\'';
+	struct STATE {
+		typedef enum {
+			NORMAL,
+			IN_COMMENT,
+			IN_SINGLE_QUOTE,
+			IN_DOUBLE_QUOTE
+		} state_t;
+	};
+	HPrepocessor const* _owner;
+	yaal::hcore::HString::const_iterator _cur;
+	STATE::state_t _state;
+	bool _escape; /*!< Is a QUOTE substate. */
+public:
+	HIterator( void )
+		: _owner( NULL ), _cur(), _state( STATE::NORMAL ), _escape( false ) {
+	}
+	HIterator( HIterator const& it_ )
+		: _owner( it_._owner ), _cur( it_._cur ), _state( it_._state ), _escape( it_._escape ) {
+	}
+	HIterator& operator = ( HIterator const& it_ ) {
+		if ( &it_ != this ) {
+			HIterator tmp( it_ );
+			swap( tmp );
+		}
+		return ( *this );
+	}
+	void swap( HIterator& it_ ) {
+		if ( &it_ != this ) {
+			using yaal::swap;
+			swap( _owner, it_._owner );
+			swap( _cur, it_._cur );
+			swap( _state, it_._state );
+			swap( _escape, it_._escape );
+		}
+		return;
+	}
+	HIterator& operator ++ ( void ) {
+		++ _cur;
+		if ( _cur != _owner->_end ) {
+			make_readable();
+		}
+		return ( *this );
+	}
+	HIterator operator ++ ( int ) const {
+		HIterator it( *this );
+		++ it;
+		return ( it );
+	}
+	bool operator == ( HIterator const& it_ ) const {
+		M_ASSERT( it_._owner == _owner );
+		return ( it_._cur == _cur );
+	}
+	bool operator != ( HIterator const& it_ ) const {
+		M_ASSERT( it_._owner == _owner );
+		return ( it_._cur != _cur );
+	}
+	value_type const& operator * ( void ) const {
+		M_ASSERT( _owner );
+		return ( *_cur );
+	}
+	yaal::hcore::HString::const_iterator raw( void ) const {
+		return ( _cur );
+	}
+private:
+	HIterator( HPrepocessor const* owner_, yaal::hcore::HString::const_iterator pos_ )
+		: _owner( owner_ ), _cur( pos_ ), _state( STATE::NORMAL ), _escape( false ) {
+		make_readable();
+		return;
+	}
+	friend class HPrepocessor;
+	void make_readable( void );
+	yaal::hcore::HString::const_iterator try_skip_whitespace( yaal::hcore::HString::const_iterator, char );
+	yaal::hcore::HString::const_iterator skip_whitespace( yaal::hcore::HString::const_iterator );
+	yaal::hcore::HString::const_iterator skip_comment( yaal::hcore::HString::const_iterator );
+};
+
+}
+
+}
+
+}
+
+#endif /* #ifndef YAAL_TOOLS_HUGINN_PREPROCESSOR_HXX_INCLUDED */
+
