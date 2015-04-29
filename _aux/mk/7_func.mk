13132f186ad46c69cfa62108f668b426971a4bb5
Marcin Konarski <marcin.konarski@codestation.org>
2015-04-29 16:58:49 +0200

    build: Allow other libraries to be clients to yaal build system.

--- _aux/mk/7_func.mk
+++ _aux/mk/7_func.mk
@@ -17,13 +17,16 @@ endif
 
 # implict pattern rule
 
-%.$$(DS): %.$$(SS)
+# $$(DIR_$(1)/)... prefix is required for other (non-yaal) libraries
+# that could be clients to this (yaal) build system.
+
+$$(DIR_$(1))/%.$$(DS): $$(DIR_SRC_$(1))/%.$$(SS)
 	@$$(call msg_always,printf "%b" "$$($$(CURR_DEP_PROGRESS_INDICATOR))$$(DEP_PROGRESS_INDICATOR_SUFFIX)$$(eval $$(call PROGRESS_INDICATOR))" && ) \
 	/bin/rm -f "$$(@)" && \
 	$$(DXX) $$(CXXFLAGS) -MM $$(<) -MT $$(@:.$$(DS)=.$$(OS)) -MT $$(@) -MF $$(@) \
 	$$(call msg,&& printf "%b" "$$(DEP_CL)")
 
-%.$$(OS): %.$$(SS)
+$$(DIR_$(1))/%.$$(OS): $$(DIR_SRC_$(1))/%.$$(SS)
 	@$$(eval $$(call IDENT_HELPER,$$(<))) $$(call msg,printf "[%3d%%] %b" "$$(shell expr $$(words $$(CUR_COUNT)) \* 100 / $$(TOTAL))" "$$($$(CURR_PROGRESS_INDICATOR))$$(eval $$(call PROGRESS_INDICATOR))Compiling \`$$(subst $$(DIR_ROOT)/,,$$(<))' ... " && ) \
 	/bin/rm -f "$$(@)" && \
 	$$(call invoke,$$(CXX) $$(CXXFLAGS) -D__ID__=$$(ID) -D__TID__=$$(TID) $$(<) -c -o $$(@) 2>&1 | tee -a make.log) && \
