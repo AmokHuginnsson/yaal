f1cb3cf846c5de6d3a37b9da306976576d9aa6e1
Marcin Konarski <marcin.konarski@codestation.org>
2015-05-28 00:06:33 +0200

    Build fix.

--- dbwrapper/mysql_driver.cxx
+++ dbwrapper/mysql_driver.cxx
@@ -343,7 +343,7 @@ HMySQLInitDeinit::HMySQLInitDeinit( void ) {
 	M_PROLOG
 	yaal_options()( "client_character_set",
 			program_options_helper::option_value( _clientCharacterSet_ ),
-			HProgramOptionsHandler::OOption::TYPE::REQUIRED,
+			HProgramOptionsHandler::OOption::ARGUMENT::REQUIRED,
 			"character set used by mysql client", "name" );
 	yaal_options().process_rc_file( "yaal", "mysql", NULL );
 #if defined( HAVE_DECL_MYSQL_AUTODETECT_CHARSET_NAME ) && ( HAVE_DECL_MYSQL_AUTODETECT_CHARSET_NAME == 1 )
