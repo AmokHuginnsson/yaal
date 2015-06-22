40d4f24e9994beaaf227d17583a045ea08a61d6d
Marcin Konarski <marcin.konarski@codestation.org>
2015-06-22 06:20:43 +0200

    HHuginn: Start work on `import' statement.

---
+++ tools/huginn/packagefactory.cxx
@@ -0,0 +1,132 @@
+/*
+---           `gameground' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+  packagefactory.cxx - this file is integral part of `gameground' project.
+
+  i.  You may not make any changes in Copyright information.
+  ii. You must attach Copyright information to any part of every copy
+      of this software.
+
+Copyright:
+
+ You can use this software free of charge and you can redistribute its binary
+ package freely but:
+  1. You are not allowed to use any part of sources of this software.
+  2. You are not allowed to redistribute any part of sources of this software.
+  3. You are not allowed to reverse engineer this software.
+  4. If you want to distribute a binary package of this software you cannot
+     demand any fees for it. You cannot even demand
+     a return of cost of the media or distribution (CD for example).
+  5. You cannot involve this software in any commercial activity (for example
+     as a free add-on to paid software or newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+#include <libintl.h>
+
+
+#include "packagefactory.hxx"
+M_VCSID( "$Id: " __ID__ " $" )
+
+using namespace yaal;
+using namespace yaal::hcore;
+using namespace yaal::tools;
+
+namespace yaal {
+
+namespace tools {
+
+namespace huginn {
+
+void HPackageCreatorInterface::initialize_globals( void ) {
+	M_PROLOG
+	do_initialize_globals();
+	return;
+	M_EPILOG
+}
+
+void HPackageCreatorInterface::cleanup_globals( void ) {
+	M_PROLOG
+	do_cleanup_globals();
+	return;
+	M_EPILOG
+}
+
+HHuginn::value_t HPackageCreatorInterface::new_instance( HHuginn* huginn_ ) {
+	M_PROLOG
+	return ( do_new_instance( huginn_ ) );
+	M_EPILOG
+}
+
+void HPackageFactory::register_package_creator( HString const& name_, HPackageCreatorInterface* instatiator_ ) {
+	M_PROLOG
+	creators_t::iterator it = _creators.find( name_ );
+	if ( it != _creators.end() )
+		M_THROW( _( "Package already registered" ), errno );
+	OCreator creator;
+	creator._instantiator = instatiator_;
+	_creators[ name_ ] = creator;
+	return;
+	M_EPILOG
+}
+
+HHuginn::value_t HPackageFactory::create_package( HHuginn* huginn_, yaal::hcore::HString const& name_ ) {
+	M_PROLOG
+	HHuginn::value_t package;
+	creators_t::iterator it = _creators.find( name_ );
+	if ( it != _creators.end() ) {
+		package = it->second._instantiator->new_instance( huginn_ );
+	}
+	return ( package );
+	M_EPILOG
+}
+
+bool HPackageFactory::is_type_valid( yaal::hcore::HString const& name_ ) {
+	M_PROLOG
+	bool valid = false;
+	creators_t::iterator it = _creators.find( name_ );
+	if ( it != _creators.end() ) {
+		valid = true;
+	}
+	return ( valid );
+	M_EPILOG
+}
+
+HPackageFactory::creators_t::iterator HPackageFactory::begin( void ) {
+	return ( _creators.begin() );
+}
+
+HPackageFactory::creators_t::iterator HPackageFactory::end( void ) {
+	return ( _creators.end() );
+}
+
+int HPackageFactory::life_time( int lifeTime_ ) {
+	return ( lifeTime_ );
+}
+
+void HPackageFactory::initialize_globals( void ) {
+	M_PROLOG
+	for ( creators_t::iterator it = _creators.begin(); it != _creators.end(); ++ it ) {
+		it->second._instantiator->initialize_globals();
+	}
+	return;
+	M_EPILOG
+}
+
+void HPackageFactory::cleanup_globals( void ) {
+	M_PROLOG
+	for ( creators_t::iterator it = _creators.begin(); it != _creators.end(); ++ it ) {
+		it->second._instantiator->cleanup_globals();
+	}
+	return;
+	M_EPILOG
+}
+
+}
+
+}
+
+}
+
---
+++ tools/huginn/packagefactory.hxx
@@ -0,0 +1,97 @@
+/*
+---           `gameground' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
+
+  packagefactory.hxx - this file is integral part of `gameground' project.
+
+  i.  You may not make any changes in Copyright information.
+  ii. You must attach Copyright information to any part of every copy
+      of this software.
+
+Copyright:
+
+ You can use this software free of charge and you can redistribute its binary
+ package freely but:
+  1. You are not allowed to use any part of sources of this software.
+  2. You are not allowed to redistribute any part of sources of this software.
+  3. You are not allowed to reverse engineer this software.
+  4. If you want to distribute a binary package of this software you cannot
+     demand any fees for it. You cannot even demand
+     a return of cost of the media or distribution (CD for example).
+  5. You cannot involve this software in any commercial activity (for example
+     as a free add-on to paid software or newspaper).
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
+*/
+
+/* YAAL_PRIVATE_IMPLEMENTATION_DETAIL */
+
+#ifndef YAAL_TOOLS_HUGINN_PACKAGEFACTORY_HXX_INCLUDED
+#define YAAL_TOOLS_HUGINN_PACKAGEFACTORY_HXX_INCLUDED
+
+#include "hcore/hmap.hxx"
+#include "hcore/hpointer.hxx"
+#include "hcore/hsingleton.hxx"
+
+#include "tools/hhuginn.hxx"
+
+namespace yaal {
+
+namespace tools {
+
+namespace huginn {
+
+class HPackageCreatorInterface {
+protected:
+	virtual void do_initialize_globals( void ){};
+	virtual void do_cleanup_globals( void ){};
+	virtual HHuginn::value_t do_new_instance( HHuginn* ) = 0;
+public:
+	virtual ~HPackageCreatorInterface( void ){}
+	void initialize_globals( void );
+	void cleanup_globals( void );
+	HHuginn::value_t new_instance( HHuginn* );
+};
+
+class HPackageFactory {
+public:
+	typedef HPackageFactory this_type;
+	struct OCreator {
+		OCreator( void )
+			: _instantiator( NULL ) {
+		}
+		OCreator( OCreator const& ) = default;
+		OCreator& operator = ( OCreator const& ) = default;
+		HPackageCreatorInterface* _instantiator;
+	};
+	typedef yaal::hcore::HMap<yaal::hcore::HString, OCreator> creators_t;
+private:
+	creators_t _creators;
+public:
+	void register_package_creator( yaal::hcore::HString const&, HPackageCreatorInterface* );
+	HHuginn::value_t create_package( HHuginn*, yaal::hcore::HString const& );
+	bool is_type_valid( yaal::hcore::HString const& );
+	creators_t::iterator begin( void );
+	creators_t::iterator end( void );
+	void initialize_globals( void );
+	void cleanup_globals( void );
+private:
+	HPackageFactory( void )
+		: _creators() {
+	}
+	~HPackageFactory( void ) {}
+	static int life_time( int );
+	friend class yaal::hcore::HSingleton<HPackageFactory>;
+	friend class yaal::hcore::HDestructor<HPackageFactory>;
+};
+
+typedef yaal::hcore::HSingleton<HPackageFactory> HPackageFactoryInstance;
+
+}
+
+}
+
+}
+
+#endif /* not YAAL_TOOLS_HUGINN_PACKAGEFACTORY_HXX_INCLUDED */
+
