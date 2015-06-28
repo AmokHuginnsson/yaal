f46be197aa99b7bf3a1f73a1966ac4c1deb3e930
Marcin Konarski <marcin.konarski@codestation.org>
2015-06-28 20:53:00 +0200

    build: Enable commit-id for clients.

--- _aux/inc-helper-pre.mk
+++ _aux/inc-helper-pre.mk
@@ -1,5 +1,6 @@
 include $(DIR_ROOT)/_aux/mk/20_term.mk
 include $(DIR_ROOT)/_aux/mk/30_suffix.mk
+include $(DIR_ROOT)/_aux/mk/40_commit-id.mk
 include $(DIR_ROOT)/_aux/mk/50_wrapper.mk
 include $(DIR_ROOT)/_aux/mk/60_flags.mk
 
