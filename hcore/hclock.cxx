AmokHuginnsson/yaal

commit f5c497ca4f57b28f7fe3694aff0196fc25be39e1

Fix build on 32bit platforms.

hcore/hclock.cxx
@@ -80,7 +80,7 @@ i64_t HClock::get_time_elapsed( yaal::hcore::time::UNIT unit_ ) const {
 	time.tv_sec = static_cast<time_t>( now.tv_sec - _moment[ IDX_SECONDS ] );
 	if ( now.tv_nsec < _moment[ IDX_NANOSECONDS ] ) {
 		-- time.tv_sec;
-		time.tv_nsec = si::NANO_IN_WHOLE - static_cast<int long>( _moment[ IDX_NANOSECONDS ] - now.tv_nsec );
+		time.tv_nsec = static_cast<int long>( ( si::NANO_IN_WHOLE - _moment[ IDX_NANOSECONDS ] ) - now.tv_nsec );
 	} else {
 		time.tv_nsec = now.tv_nsec - static_cast<int long>( _moment[ IDX_NANOSECONDS ] );
 	}

hcore/hchunk.hxx
@@ -64,25 +64,32 @@ public:
 	HChunk& operator = ( HChunk&& );
 	~HChunk( void );
 	template<typename tType>
-	tType const* get() const
-		{ return ( static_cast<tType const*>( _data ) ); }
+	tType const* get() const {
+		return ( static_cast<tType const*>( _data ) );
+	}
 	template<typename tType>
-	tType* get()
-		{ return ( static_cast<tType*>( _data ) ); }
-	void const* raw( void ) const
-		{ return ( static_cast<char const*>( _data ) ); }
-	void* raw( void )
-		{ return ( static_cast<char*>( _data ) ); }
+	tType* get() {
+		return ( static_cast<tType*>( _data ) );
+	}
+	void const* raw( void ) const {
+		return ( static_cast<char const*>( _data ) );
+	}
+	void* raw( void ) {
+		return ( static_cast<char*>( _data ) );
+	}
 	void free( void );
 	void* realloc( int long, STRATEGY::enum_t = STRATEGY::GEOMETRIC );
 	void swap( HChunk& );
-	int long get_size( void ) const
-		{ return ( _size ); }
-	int long size( void ) const
-		{ return ( _size ); }
+	int long get_size( void ) const {
+		return ( _size );
+	}
+	int long size( void ) const {
+		return ( _size );
+	}
 	template<typename T>
-	int long count_of( void ) const
-		{ return ( _size / static_cast<int>( sizeof ( T ) ) ); }
+	int long count_of( void ) const {
+		return ( _size / static_cast<int>( sizeof ( T ) ) );
+	}
 private:
 	HChunk( HChunk const& ) = delete;
 	HChunk& operator = ( HChunk const& ) = delete;
@@ -93,8 +100,9 @@ int long chunk_size( int long count_ ) {
 	return ( static_cast<int>( sizeof ( T ) ) * count_ );
 }
 
-inline void swap( yaal::hcore::HChunk& a, yaal::hcore::HChunk& b )
-	{ a.swap( b ); }
+inline void swap( yaal::hcore::HChunk& a, yaal::hcore::HChunk& b ) {
+	a.swap( b );
+}
 
 }
 

hcore/hhashcontainer.cxx
@@ -57,7 +57,7 @@ int long hash<HString>::operator () ( HString const& string_ ) const {
 
 template<>
 int long hash<double long>::operator () ( double long const& val_ ) const {
-	return ( reinterpret_cast<int long long const&>( static_cast<double const&>( val_ ) ) );
+	return ( static_cast<int long>( reinterpret_cast<int long long const&>( static_cast<double const&>( val_ ) ) ) );
 }
 
 template<typename T>

hcore/hthread.cxx
@@ -480,7 +480,7 @@ HCondition::status_t HCondition::do_wait( time_t timeOutSeconds_,
 
 HCondition::status_t HCondition::wait_until( HTime const& time_ ) {
 	M_PROLOG
-	return ( do_wait( time_.raw() - HTime::SECONDS_TO_UNIX_EPOCH, 0 ) );
+	return ( do_wait( static_cast<time_t>( time_.raw() - HTime::SECONDS_TO_UNIX_EPOCH ), 0 ) );
 	M_EPILOG
 }
 
@@ -489,11 +489,11 @@ HCondition::status_t HCondition::wait_for( time::duration_t duration_ ) {
 	timespec timeOut;
 	::memset( &timeOut, 0, sizeof ( timeOut ) );
 	M_ENSURE( clock_gettime( FWD_CLOCK_REALTIME, &timeOut ) == 0 );
-	timeOut.tv_sec += ( duration_ / si::NANO_IN_WHOLE ).raw();
-	timeOut.tv_nsec += ( duration_ % si::NANO_IN_WHOLE ).raw();
+	timeOut.tv_sec += static_cast<time_t>( ( duration_ / si::NANO_IN_WHOLE ).raw() );
+	timeOut.tv_nsec += static_cast<int long>( ( duration_ % si::NANO_IN_WHOLE ).raw() );
 	if ( timeOut.tv_nsec >= si::NANO_IN_WHOLE ) {
 		++ timeOut.tv_sec;
-		timeOut.tv_nsec -= si::NANO_IN_WHOLE;
+		timeOut.tv_nsec -= static_cast<int long>( si::NANO_IN_WHOLE );
 	}
 	return ( do_wait( timeOut.tv_sec, timeOut.tv_nsec ) );
 	M_EPILOG

tools/halarm.cxx
@@ -79,7 +79,7 @@ HAlarm::HAlarm( int long miliseconds_ )
 		itimerspec timeout;
 		::memset( &timeout, 0, sizeof ( timeout ) );
 		timeout.it_value.tv_sec = miliseconds_ / si::MILI_IN_WHOLE;
-		timeout.it_value.tv_nsec = ( miliseconds_ % si::MILI_IN_WHOLE ) * si::NANO_IN_MILI;
+		timeout.it_value.tv_nsec = static_cast<int long>( ( miliseconds_ % si::MILI_IN_WHOLE ) * si::NANO_IN_MILI );
 		M_ENSURE( timer_settime( *t, 0, &timeout, NULL ) == 0 );
 		++ step;
 	} catch ( ... ) {

tools/hash.cxx
@@ -74,8 +74,8 @@ yaal::hcore::HString md5( HStreamInterface& stream ) {
 			u32_t* x = static_cast<u32_t*>( block.data() );
 			HBitmap bmp;
 			bmp.use( block.data(), BLOCK_SIZE );
-			bmp.fill( last, BLOCK_SIZE - last, false );
-			bmp.set( last, true );
+			bmp.fill( static_cast<int long>( last ), static_cast<int long>( BLOCK_SIZE - last ), false );
+			bmp.set( static_cast<int long>( last ), true );
 			if ( last >= ( ( BLOCK_SIZE - MESSAGE_LENGTH_SIZE ) - SUPPLEMENT_SIZE ) ) {
 				update_md5_state( state, block );
 				yaal::fill( x, x + ( BLOCK_SIZE >> 5 ), 0u );
@@ -135,8 +135,8 @@ yaal::hcore::HString sha1( HStreamInterface& stream ) {
 			u32_t* x = static_cast<u32_t*>( block.data() );
 			HBitmap bmp;
 			bmp.use( block.data(),BLOCK_SIZE );
-			bmp.fill( last, BLOCK_SIZE - last, false );
-			bmp.set( last, true );
+			bmp.fill( static_cast<int long>( last ), static_cast<int long>( BLOCK_SIZE - last ), false );
+			bmp.set( static_cast<int long>( last ), true );
 			if ( last >= ( ( BLOCK_SIZE - MESSAGE_LENGTH_SIZE ) - SUPPLEMENT_SIZE ) ) {
 				update_sha1_state( state, block );
 				yaal::fill( x, x + ( BLOCK_SIZE >> 5 ), 0u );

tools/hdes.cxx
@@ -160,11 +160,10 @@ void HDes::_des( u32_t* block_, int action_, int part_ ) {
 				^ reinterpret_cast<u16_t*>( endKey )[ 2 ] );
 		for ( int ctr( 0 ); ctr < DES::BLOCK_SIZE; ctr ++ ) {
 			col = row = 0;
-			rowBmp.set( 6, src.get( static_cast<u32_t>( ctr ) * 6 ) );
-			rowBmp.set( 7, src.get( static_cast<u32_t>( ctr ) * 6 + 5 ) );
+			rowBmp.set( 6, src.get( ctr * 6 ) );
+			rowBmp.set( 7, src.get( ctr * 6 + 5 ) );
 			for ( int i( 0 ); i < 4; ++ i )
-				colBmp.set( static_cast<u32_t>( i ) + 4,
-						src.get( static_cast<u32_t>( ctr * 6 + i + 1 ) ) );
+				colBmp.set( i + 4, src.get( ctr * 6 + i + 1 ) );
 			u8_t mask( _sBlock_[ ctr ][ row ][ col ] );
 			if ( ! ( ctr & 1 ) )
 				mask = static_cast<u8_t>( mask << 4 );
@@ -191,8 +190,7 @@ void HDes::permutate( u8_t* buffer_, const u8_t* tab_, int len_ ) const {
 	src.use( buffer_, DES::BLOCK_SIZE * DES::BITS_IN_BYTE );
 	dst.use( bufTmp, DES::BLOCK_SIZE * DES::BITS_IN_BYTE );
 	for ( ctr = 0; ctr < len_; ctr ++ )
-		dst.set( static_cast<u32_t>( ctr ),
-				src.get( static_cast<u32_t>( tab_[ ctr ] ) ) );
+		dst.set( ctr, src.get( tab_[ ctr ] ) );
 	::memcpy( buffer_, bufTmp, DES::BLOCK_SIZE );
 	return;
 }

tools/huginn/helper.cxx
@@ -195,7 +195,7 @@ yaal::hcore::HString const& get_string( HHuginn::value_t const& value_ ) {
 	return ( static_cast<HHuginn::HString const*>( value_.raw() )->value() );
 }
 
-int long get_integer( HHuginn::value_t const& value_ ) {
+int long long get_integer( HHuginn::value_t const& value_ ) {
 	M_ASSERT( !! value_ );
 	M_ASSERT( dynamic_cast<HHuginn::HInteger const*>( value_.raw() ) );
 	return ( static_cast<HHuginn::HInteger const*>( value_.raw() )->value() );

commit df83233bce911dca444ab8b931fb1ac1527f7f60

Fix bug introduced by f5c497ca4f57b28f7fe3694aff0196fc25be39e1.

hcore/hclock.cxx
@@ -80,7 +80,7 @@ i64_t HClock::get_time_elapsed( yaal::hcore::time::UNIT unit_ ) const {
 	time.tv_sec = static_cast<time_t>( now.tv_sec - _moment[ IDX_SECONDS ] );
 	if ( now.tv_nsec < _moment[ IDX_NANOSECONDS ] ) {
 		-- time.tv_sec;
-		time.tv_nsec = static_cast<int long>( ( si::NANO_IN_WHOLE - _moment[ IDX_NANOSECONDS ] ) - now.tv_nsec );
+		time.tv_nsec = static_cast<int long>( si::NANO_IN_WHOLE - ( _moment[ IDX_NANOSECONDS ] - now.tv_nsec ) );
 	} else {
 		time.tv_nsec = now.tv_nsec - static_cast<int long>( _moment[ IDX_NANOSECONDS ] );
 	}