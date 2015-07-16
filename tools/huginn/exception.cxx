82ef3dd89a0aa5a9f3beee61d5ee42531d6b020b
Marcin Konarski <marcin.konarski@codestation.org>
2015-07-17 01:23:56 +0200

    HHuginn: Built-in exceptions show correct type names in internal error messages.

--- tools/huginn/exception.cxx
+++ tools/huginn/exception.cxx
@@ -44,54 +44,63 @@ namespace huginn {
 
 namespace exception {
 
-inline HHuginn::value_t what( huginn::HThread*, HHuginn::HObject* object_, HHuginn::values_t const& values_, int position_ ) {
-	M_PROLOG
-	char const name[] = "Exception.what";
-	verify_arg_count( name, values_, 0, 0, position_ );
-	return ( make_pointer<HHuginn::HString>( static_cast<HHuginn::HException*>( object_ )->what() ) );
-	M_EPILOG
-}
-
 class HExceptionClass : public HHuginn::HClass {
 public:
 	typedef HExceptionClass this_type;
 	typedef HHuginn::HClass base_type;
-	HExceptionClass( HHuginn::type_t type_, HHuginn::HClass::field_names_t const& fieldNames_, HHuginn::values_t const& defaults_ )
-		: HHuginn::HClass( nullptr, type_, nullptr, fieldNames_, defaults_ ) {
+	HExceptionClass(
+		HHuginn* huginn_,
+		yaal::hcore::HString const& name_,
+		HHuginn::HClass const* base_
+	) : HHuginn::HClass(
+			huginn_,
+			HHuginn::HType::register_type( name_, huginn_ ),
+			base_,
+			base_
+				? HHuginn::HClass::field_names_t{}
+				: HHuginn::HClass::field_names_t{
+					"what",
+					"message"
+				},
+			base_
+				? HHuginn::values_t{}
+				: HHuginn::values_t{
+					make_pointer<HHuginn::HClass::HMethod>( hcore::call( &HExceptionClass::what, _1, _2, _3, _4 ) ),
+					make_pointer<HHuginn::HClass::HMethod>( hcore::call( &HExceptionClass::what, _1, _2, _3, _4 ) )
+				}
+		) {
 		return;
 	}
 private:
 	virtual HHuginn::value_t do_create_instance( huginn::HThread*, HHuginn::values_t const& values_, int position_ ) const {
 		M_PROLOG
-		verify_arg_count( "Exception.contructor", values_, 1, 1, position_ );
+		HString n( name() + ".contructor" );
+		verify_arg_count( n, values_, 1, 1, position_ );
+		verify_arg_type( n, values_, 0, HHuginn::TYPE::STRING, true, position_ );
 		return ( make_pointer<HHuginn::HException>( this, get_string( values_[0] ) ) );
+		M_EPILOG
+	}
+	static HHuginn::value_t what( huginn::HThread*, HHuginn::HObject* object_, HHuginn::values_t const& values_, int position_ ) {
+		M_PROLOG
+		HHuginn::HException* e( static_cast<HHuginn::HException*>( object_ ) );
+		verify_arg_count(
+			static_cast<HExceptionClass const*>( e->get_class() )->name() + ".what",
+			values_, 0, 0, position_
+		);
+		return ( make_pointer<HHuginn::HString>( e->what() ) );
 		M_EPILOG
 	}
 };
 
-HHuginn::class_t _class_(
-	make_pointer<HExceptionClass>(
-		HHuginn::HType::register_type( "Exception", nullptr ),
-		HHuginn::HClass::field_names_t{
-			"what",
-			"message"
-		},
-		HHuginn::values_t{
-			make_pointer<HHuginn::HClass::HMethod>( hcore::call( &exception::what, _1, _2, _3, _4 ) ),
-			make_pointer<HHuginn::HClass::HMethod>( hcore::call( &exception::what, _1, _2, _3, _4 ) )
-		}
-	)
-);
+HHuginn::class_t _class_( make_pointer<HExceptionClass>( nullptr, "Exception", nullptr ) );
 
 HHuginn::class_t create_class( HHuginn* huginn_, yaal::hcore::HString const& name_, HHuginn::HClass const* base_ ) {
 	M_PROLOG
 	HHuginn::class_t c(
-		make_pointer<HHuginn::HClass>(
+		make_pointer<HExceptionClass>(
 			huginn_,
-			HHuginn::HType::register_type( name_, huginn_ ),
-			base_ ? base_ : _class_.raw(),
-			HHuginn::HClass::field_names_t{},
-			HHuginn::values_t{}
+			name_,
+			base_ ? base_ : _class_.raw()
 		)
 	);
 	return ( c );
