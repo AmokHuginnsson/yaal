b032497440dfc7f4191aad2c51fe55f04925c23f
Marcin Konarski <marcin.konarski@codestation.org>
2016-06-25 02:02:25 +0200

    lcov: Ignore M_EPILOG stanzas.

--- _aux/mk/e0_target-tools.mk
+++ _aux/mk/e0_target-tools.mk
@@ -4,7 +4,7 @@ tags: $(SRCS) $(HDRS)
 	printf "%b\n" "done."
 
 DIR_COVERAGE=build/cov
-LCOV_PARAMS=
+LCOV_PARAMS=--rc lcov_excl_line='\bM_EPILOG\b'
 
 prepare-coverage-baseline: $(DIR_COVERAGE)
 	@cd $(DIR_ROOT) && \
