d6fadbc8e9f6376852639a9e500fb70b42459ee8
Marcin Konarski <marcin.konarski@codestation.org>
2016-07-13 04:20:32 +0200

    Work on msvcxx port (11).

--- _aux/msvcxx/sys/wait.h
+++ _aux/msvcxx/sys/wait.h
@@ -1,12 +1,36 @@
 #ifndef YAAL_MSVCXX_SYS_WAIT_H_INCLUDED
 #define YAAL_MSVCXX_SYS_WAIT_H_INCLUDED 1
 
+static int const WNOHANG   = 1;
+static int const WUNTRACED = 2;
+
 namespace msvcxx {
 int waitpid( int, int*, int );
 }
 
-inline int waitpid( int pid_, int* status_, int options_ )
-	{ return ( msvcxx::waitpid( pid_, status_, options_ ) ); }
+inline int waitpid( int pid_, int* status_, int options_ ) {
+	return ( msvcxx::waitpid( pid_, status_, options_ ) );
+}
+
+constexpr int WEXITSTATUS( int status_ ) {
+	return ( ( status_ & 0xff00 ) >> 8 );
+}
+
+constexpr int WTERMSIG( int status_ ) {
+	return ( status_ & 0x7f );
+}
+
+constexpr bool WIFEXITED( int status_ ) {
+	return ( WTERMSIG( status_ ) == 0  );
+}
+
+constexpr bool WIFSTOPPED( int status_ ) {
+	return ( ( status_ & 0xff ) == 0x7f );
+}
+
+constexpr bool WIFSIGNALED( int status_ ) {
+	return ( ! ( WIFSTOPPED( status_ ) || WIFEXITED( status_ ) ) );
+}
 
 #endif /* not YAAL_MSVCXX_SYS_WAIT_H_INCLUDED */
 
---
+++ _aux/msvcxx/termios.h
@@ -0,0 +1,91 @@
+#ifndef YAAL_MSVCXX_TERMIOS_H_INCLUDED
+#define YAAL_MSVCXX_TERMIOS_H_INCLUDED 1
+
+typedef int long speed_t;
+typedef int long unsigned tcflag_t;
+typedef char unsigned cc_t;
+
+static int const TCSANOW   = 0;
+static int const TCSAFLUSH = 2;
+
+static int const TCIFLUSH  = 1;
+static int const TCOFLUSH  = 2;
+static int const TCIOFLUSH = 3;
+
+static tcflag_t const IGNBRK = 1 << 0;
+static tcflag_t const IGNPAR = 1 << 2;
+static tcflag_t const INPCK  = 1 << 4;
+static tcflag_t const ICRNL  = 1 << 8;
+static tcflag_t const IXON   = 1 << 9;
+static tcflag_t const IXOFF  = 1 << 10;
+static tcflag_t const IXANY  = 1 << 11;
+
+static tcflag_t const CS5        = 0;
+static tcflag_t const CS6        = 1 << 8;
+static tcflag_t const CS7        = 1 << 9;
+static tcflag_t const CS8        = CS6 | CS7;
+static tcflag_t const CSIZE      = CS5 | CS6 | CS7 | CS8;
+static tcflag_t const CSTOPB     = 1 << 10;
+static tcflag_t const CREAD      = 1 << 11;
+static tcflag_t const PARENB     = 1 << 12;
+static tcflag_t const PARODD     = 1 << 13;
+static tcflag_t const CCTS_OFLOW = 1 << 16;
+static tcflag_t const CRTS_IFLOW = 1 << 17;
+static tcflag_t const CRTSCTS    = CCTS_OFLOW | CRTS_IFLOW;
+
+static tcflag_t const ECHO   = 1 << 3;
+static tcflag_t const ICANON = 1 << 8;
+static tcflag_t const IEXTEN = 1 << 10;
+
+static int const VEOF     = 0;
+static int const VEOL     = 1;
+static int const VEOL2    = 2;
+static int const VERASE   = 3;
+static int const VWERASE  = 4;
+static int const VKILL    = 5;
+static int const VREPRINT = 6;
+static int const VINTR    = 8;
+static int const VQUIT    = 9;
+static int const VSUSP    = 10;
+static int const VSTART   = 12;
+static int const VSTOP    = 13;
+static int const VLNEXT   = 14;
+static int const VDISCARD = 15;
+static int const VMIN     = 16;
+static int const VTIME    = 17;
+
+static speed_t const B230400 = 230400;
+static speed_t const B115200 = 115200;
+static speed_t const B57600  = 57600;
+static speed_t const B38400  = 38400;
+static speed_t const B19200  = 19200;
+static speed_t const B9600   = 9600;
+static speed_t const B4800   = 4800;
+static speed_t const B2400   = 2400;
+
+struct termios {
+  tcflag_t c_iflag;
+  tcflag_t c_oflag;
+  tcflag_t c_cflag;
+  tcflag_t c_lflag;
+	static int const NCCS = 20;
+  cc_t c_cc[NCCS];
+};
+
+int tcgetattr( int, struct termios* );
+int cfsetospeed( struct termios*, speed_t );
+int cfsetispeed( struct termios*, speed_t );
+int tcsetattr( int, int, struct termios const* );
+int tcflush( int, int );
+int tcdrain( int );
+int tcsendbreak( int, int );
+
+/*
+speed_t cfgetospeed( struct termios const* );
+speed_t cfgetispeed( struct termios const* );
+int cfsetspeed( struct termios*, speed_t );
+void cfmakeraw( struct termios* );
+*/
+
+#endif /* not YAAL_MSVCXX_TERMIOS_H_INCLUDED */
+
