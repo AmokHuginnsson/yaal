4ef5aa0f2e0a030a8654e85c4d8b90a478669b43
Marcin Konarski <marcin.konarski@codestation.org>
2018-05-06 08:48:16 +0200

    bit: Add 32, 16 and 8 bit versions of `bit count` and `bit reverse`.

--- hcore/bit.hxx
+++ hcore/bit.hxx
@@ -15,13 +15,31 @@ namespace hcore {
 namespace bit {
 
 static int const SHIFT[] = { 1, 2, 4, 8, 16, 32 };
-static u64_t const BIT_MASK[] = {
+static u64_t const BIT_MASK64[] = {
 	0x5555555555555555,
 	0x3333333333333333,
 	0x0f0f0f0f0f0f0f0f,
 	0x00ff00ff00ff00ff,
 	0x0000ffff0000ffff,
 	0x00000000ffffffff
+};
+static u32_t const BIT_MASK32[] = {
+	0x55555555,
+	0x33333333,
+	0x0f0f0f0f,
+	0x00ff00ff,
+	0x0000ffff
+};
+static u16_t const BIT_MASK16[] = {
+	0x5555,
+	0x3333,
+	0x0f0f,
+	0x00ff
+};
+static u8_t const BIT_MASK8[] = {
+	0x55,
+	0x33,
+	0x0f
 };
 
 static const int MULTIPLY_DE_BRUIJN_BIT_POSITION[64] = {
@@ -32,15 +50,57 @@ static const int MULTIPLY_DE_BRUIJN_BIT_POSITION[64] = {
 };
 
 inline int count( u64_t v_ ) {
-	for ( int i( 0 ); i < countof ( SHIFT ); ++ i ) {
-		v_ = ( v_ & BIT_MASK[i] ) + ( ( v_ >> SHIFT[i] ) & BIT_MASK[i] );
+	for ( int i( 0 ); i < countof ( BIT_MASK64 ); ++ i ) {
+		v_ = ( v_ & BIT_MASK64[i] ) + ( ( v_ >> SHIFT[i] ) & BIT_MASK64[i] );
 	}
 	return ( static_cast<int>( v_ ) );
 }
 
 inline u64_t reverse( u64_t v_ ) {
-	for ( int i( 0 ); i < countof ( SHIFT ); ++ i ) {
-		v_ = ( ( v_ >> SHIFT[i] ) & BIT_MASK[i] ) | ( ( v_ & BIT_MASK[i] ) << SHIFT[i] );
+	for ( int i( 0 ); i < countof ( BIT_MASK64 ); ++ i ) {
+		v_ = ( ( v_ >> SHIFT[i] ) & BIT_MASK64[i] ) | ( ( v_ & BIT_MASK64[i] ) << SHIFT[i] );
+	}
+	return ( v_ );
+}
+
+inline int count( u32_t v_ ) {
+	for ( int i( 0 ); i < countof ( BIT_MASK32 ); ++ i ) {
+		v_ = ( v_ & BIT_MASK32[i] ) + ( ( v_ >> SHIFT[i] ) & BIT_MASK32[i] );
+	}
+	return ( static_cast<int>( v_ ) );
+}
+
+inline u32_t reverse( u32_t v_ ) {
+	for ( int i( 0 ); i < countof ( BIT_MASK32 ); ++ i ) {
+		v_ = ( ( v_ >> SHIFT[i] ) & BIT_MASK32[i] ) | ( ( v_ & BIT_MASK32[i] ) << SHIFT[i] );
+	}
+	return ( v_ );
+}
+
+inline int count( u16_t v_ ) {
+	for ( int i( 0 ); i < countof ( BIT_MASK16 ); ++ i ) {
+		v_ = static_cast<u16_t>( ( v_ & BIT_MASK16[i] ) + ( ( v_ >> SHIFT[i] ) & BIT_MASK16[i] ) );
+	}
+	return ( static_cast<int>( v_ ) );
+}
+
+inline u16_t reverse( u16_t v_ ) {
+	for ( int i( 0 ); i < countof ( BIT_MASK16 ); ++ i ) {
+		v_ = static_cast<u16_t>( ( ( v_ >> SHIFT[i] ) & BIT_MASK16[i] ) | ( ( v_ & BIT_MASK16[i] ) << SHIFT[i] ) );
+	}
+	return ( v_ );
+}
+
+inline int count( u8_t v_ ) {
+	for ( int i( 0 ); i < countof ( BIT_MASK8 ); ++ i ) {
+		v_ = static_cast<u8_t>( ( v_ & BIT_MASK8[i] ) + ( ( v_ >> SHIFT[i] ) & BIT_MASK8[i] ) );
+	}
+	return ( static_cast<int>( v_ ) );
+}
+
+inline u8_t reverse( u8_t v_ ) {
+	for ( int i( 0 ); i < countof ( BIT_MASK8 ); ++ i ) {
+		v_ = static_cast<u8_t>( ( ( v_ >> SHIFT[i] ) & BIT_MASK8[i] ) | ( ( v_ & BIT_MASK8[i] ) << SHIFT[i] ) );
 	}
 	return ( v_ );
 }
