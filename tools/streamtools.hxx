2636f7d30db0212a501b5ba0d6b82123d95d44d8
Marcin Konarski <marcin.konarski@codestation.org>
2018-09-07 00:45:27 +0200

    streamtools: Support text/bin (de)serialization of HList<>s.

--- tools/streamtools.hxx
+++ tools/streamtools.hxx
@@ -299,8 +299,8 @@ yaal::hcore::HStreamInterface& write( yaal::hcore::HStreamInterface& out, yaal::
 template<typename tType, typename allocator_t>
 yaal::hcore::HStreamInterface& read( yaal::hcore::HStreamInterface& out, yaal::hcore::HDeque<tType, allocator_t>& d_ ) {
 	M_PROLOG
-	typedef yaal::hcore::HDeque<tType, allocator_t> array_t;
-	return ( container_scan( out, d_, static_cast<void ( array_t::* )( typename array_t::value_type const& )>( &array_t::push_back ), "deque" ) );
+	typedef yaal::hcore::HDeque<tType, allocator_t> deque_t;
+	return ( container_scan( out, d_, static_cast<void ( deque_t::* )( typename deque_t::value_type const& )>( &deque_t::push_back ), "deque" ) );
 	M_EPILOG
 }
 
@@ -308,6 +308,14 @@ template<typename tType, typename allocator_t>
 yaal::hcore::HStreamInterface& write( yaal::hcore::HStreamInterface& out, yaal::hcore::HList<tType, allocator_t> const& l_ ) {
 	M_PROLOG
 	return ( container_dump( out, l_, "list" ) );
+	M_EPILOG
+}
+
+template<typename tType, typename allocator_t>
+yaal::hcore::HStreamInterface& read( yaal::hcore::HStreamInterface& out, yaal::hcore::HList<tType, allocator_t>& l_ ) {
+	M_PROLOG
+	typedef yaal::hcore::HList<tType, allocator_t> list_t;
+	return ( container_scan( out, l_, static_cast<void ( list_t::* )( typename list_t::value_type const& )>( &list_t::push_back ), "list" ) );
 	M_EPILOG
 }
 
@@ -678,6 +686,14 @@ yaal::hcore::HStreamInterface& operator << ( yaal::hcore::HStreamInterface& out,
 	M_PROLOG
 	using tools::stream::write;
 	return ( write( out, l_ ) );
+	M_EPILOG
+}
+
+template<typename tType, typename allocator_t>
+yaal::hcore::HStreamInterface& operator >> ( yaal::hcore::HStreamInterface& in, yaal::hcore::HList<tType, allocator_t>& l_ ) {
+	M_PROLOG
+	using tools::stream::read;
+	return ( read( in, l_ ) );
 	M_EPILOG
 }
 
