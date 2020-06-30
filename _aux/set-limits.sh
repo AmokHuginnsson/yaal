2867fca619258011a8bc96a26d0fb2ed831e90f0
Marcin Konarski <marcin.konarski@codestation.org>
2020-06-30 22:06:11 +0200

    _aux: Limit memory availavle for tests on 64bit machines with more then 16GiB of RAM.

--- _aux/set-limits.sh
+++ _aux/set-limits.sh
@@ -1,24 +1,24 @@
 ### ! /bin/sh
 
 PHYS_MEM=0
-OSTYPE=`uname -s`
+OSTYPE=$(uname -s)
 
 case "x${OSTYPE}" in
 	x*Linux)
-		PHYS_MEM=`free -m | awk '/^Mem:/{print $2}'`
+		PHYS_MEM=$(free -m | awk '/^Mem:/{print $2}')
 	;;
 	xFreeBSD)
-		PHYS_MEM=`/sbin/sysctl -n hw.physmem | awk '{print int( $1 / 1024 / 1024 )}'`
+		PHYS_MEM=$(/sbin/sysctl -n hw.physmem | awk '{print int( $1 / 1024 / 1024 )}')
 	;;
 	xSolaris|xSunOS)
-		PHYS_MEM=`prtconf | awk '/Memory/{print $3}'`
+		PHYS_MEM=$(prtconf | awk '/Memory/{print $3}')
 	;;
 	xDarwin)
-		PHYS_MEM=`/usr/sbin/sysctl -n hw.memsize | awk '{print int( $1 / 1024 / 1024 )}'`
+		PHYS_MEM=$(/usr/sbin/sysctl -n hw.memsize | awk '{print int( $1 / 1024 / 1024 )}')
 	;;
 esac
 
-PHYS_MEM=`expr "${PHYS_MEM}" \* 1024`
+PHYS_MEM=$(expr "${PHYS_MEM}" \* 1024)
 
 ulimit -c unlimited
 
@@ -27,7 +27,7 @@ if [ -z "${PROC_LIMIT}" ] ; then
 fi
 
 # bash proc limit is set with -u, dash proc limit is set with -p
-IS_BASH=`ulimit -a | grep 'max user processes'`
+IS_BASH=$(ulimit -a | grep 'max user processes')
 if [ "x${IS_BASH}" != "x" ] ; then
 	ulimit -u ${PROC_LIMIT} > /dev/null 2>&1 || true
 else
@@ -38,12 +38,18 @@ ulimit -s 8192
 LIMITED=$(ulimit -v)
 if [ ${PHYS_MEM} -ne 0 -a -z "${LIMITED}" ] ; then
 	if [ -z "${MAX_32BIT_PHYS_MEM}" ] ; then
-		MAX_32BIT_PHYS_MEM=`expr 4095 \* 1024`
+		MAX_32BIT_PHYS_MEM=$(expr 4095 \* 1024)
 	fi
 	LIMITED=$(ulimit -v ${PHYS_MEM} ; ulimit -v)
 	if [ \( ${PHYS_MEM} -gt ${MAX_32BIT_PHYS_MEM} \) -a \( "x${LIMITED}" = "xunlimited" \) ] ; then
 		PHYS_MEM=${MAX_32BIT_PHYS_MEM}
 	fi
+	ulimit -v ${PHYS_MEM}
+	ulimit -d ${PHYS_MEM}
+fi
+MAX_64BIT_PHYS_MEM=$(expr 16 \* 1024 \* 1024 - 1)
+if [ ${PHYS_MEM} -gt ${MAX_64BIT_PHYS_MEM} ] ; then
+	PHYS_MEM=${MAX_64BIT_PHYS_MEM}
 	ulimit -v ${PHYS_MEM}
 	ulimit -d ${PHYS_MEM}
 fi
