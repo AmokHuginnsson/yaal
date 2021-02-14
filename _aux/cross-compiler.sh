d72eff541bc380b8f0b88dc47de9d28269c9d1c7
Marcin Konarski <marcin.konarski@codestation.org>
2021-02-14 20:55:53 +0100

    cross-compiler.sh: Modernize script.

--- _aux/cross-compiler.sh
+++ _aux/cross-compiler.sh
@@ -3,66 +3,67 @@
 # Set cross compilation build environment.
 
 if [ "x${1}" == "x" ] ; then
-	echo "You must specify toolchain directory with TOOLCHAIN={dir}."
+	echo "You must specify toolchain directory with toolchain={dir}."
 	return
 fi
 
-TOOLCHAIN=`echo "${1}" | awk -F '=' '{print $1}'`
+toolchain="$(echo "${1}" | awk -F '=' '{print $1}')"
 
-if [ "x${TOOLCHAIN}" != "xTOOLCHAIN" ] ; then
-	echo "First argument must be TOOLCHAIN."
+if [ "x${toolchain}" != "xtoolchain" ] ; then
+	echo "First argument must be toolchain."
 	return
 fi
 
-TOOLCHAIN=`echo "${1}" | awk -F '=' '{OFS="";$1 = "";print}'`
+toolchain="$(echo "${1}" | awk -F '=' '{OFS="";$1 = "";print}')"
 
-if [ -z "${TOOLCHAIN}" ] ; then
-	echo "TOOLCHAIN needs to have a value."
+if [ -z "${toolchain}" ] ; then
+	echo "toolchain needs to have a value."
 	return
 fi
 
-if [ ! -d "${TOOLCHAIN}" ] ; then
-	echo "TOOLCHAIN directory must exist."
+if [ ! -d "${toolchain}" ] ; then
+	echo "toolchain directory must exist."
 	return
 fi
 
 if [ "x${2}" == "x" ] ; then
-	export SYSROOT="${TOOLCHAIN}/sysroot"
+	sysroot="${toolchain}/sysroot"
 else
-	SYSROOT=`echo "${2}" | awk -F '=' '{print $1}'`
+	sysroot="$(echo "${2}" | awk -F '=' '{print $1}')"
 
-	if [ "x${SYSROOT}" != "xSYSROOT" ] ; then
-		echo "Second argument must be SYSROOT."
+	if [ "x${sysroot}" != "xsysroot" ] ; then
+		echo "Second argument must be sysroot."
 		return
 	fi
 
-	SYSROOT=`echo "${2}" | awk -F '=' '{OFS="";$1 = "";print}'`
+	sysroot="$(echo "${2}" | awk -F '=' '{OFS="";$1 = "";print}')"
 
-	if [ -z "${SYSROOT}" ] ; then
-		echo "SYSROOT needs to have a value."
+	if [ -z "${sysroot}" ] ; then
+		echo "sysroot needs to have a value."
 		return
 	fi
 
-	if [ ! -d "${SYSROOT}" ] ; then
-		echo "SYSROOT directory must exist."
+	if [ ! -d "${sysroot}" ] ; then
+		echo "sysroot directory must exist."
 		return
 	fi
 fi
 
-# set path = ( "${TOOLCHAIN}/bin" ${path} )
+# set path = ( "${toolchain}/bin" ${path} )
 
-HOST_TRIPLET=`find "${TOOLCHAIN}" -path '*/bin/*-cpp' | xargs basename | sed -e 's/-cpp//'`
+hostTriplet="$(find "${toolchain}" -path '*/bin/*-cpp' | xargs basename | sed -e 's/-cpp//')"
 
-echo "TOOLCHAIN = ${TOOLCHAIN}"
-echo "SYSROOT = ${SYSROOT}"
-echo "HOST_TRIPLET = ${HOST_TRIPLET}"
+echo "toolchain = ${toolchain}"
+echo "sysroot = ${sysroot}"
+echo "hostTriplet = ${hostTriplet}"
 
 unset PKG_CONFIG_DIR
-export PKG_CONFIG_LIBDIR="${SYSROOT}/usr/lib/${HOST_TRIPLET}/pkgconfig:${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig"
-export PKG_CONFIG_SYSROOT_DIR="${SYSROOT}"
-export CONFIGURE="--host=${HOST_TRIPLET}"
-export EXTRA_CXXFLAGS="--sysroot=${SYSROOT}"
-export EXTRA_LXXFLAGS="--sysroot=${SYSROOT}"
+export PKG_CONFIG_LIBDIR="${sysroot}/usr/lib/${hostTriplet}/pkgconfig:${sysroot}/usr/lib/pkgconfig:${sysroot}/usr/share/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="${sysroot}"
+export CONFIGURE="--host=${hostTriplet}"
+export EXTRA_CXXFLAGS="--sysroot=${sysroot}"
+export EXTRA_LXXFLAGS="--sysroot=${sysroot}"
+export SYSROOT="${sysroot}"
 unset CC
 unset CXX
 
